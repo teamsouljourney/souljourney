2b73bda7e7cc53c6a376f0a9588bbf6319122364
ucangun <149247682+ucangun@users.noreply.github.com>
2025-01-21 22:03:26 +0100

    refactor: refactor Contact page layout and styles

--- src/components/Contact/ContactHeader.jsx
+++ src/components/Contact/ContactHeader.jsx
@@ -1,8 +1,10 @@
 const ContactHeader = () => {
   return (
-    <div>
-      <h1>Get in Touch</h1>
-      <p>
+    <div className="px-4 mb-8 text-center md:px-8">
+      <h1 className="mb-4 text-3xl font-bold md:text-5xl text-pink-dark">
+        Get in Touch
+      </h1>
+      <p className="max-w-2xl mx-auto text-base leading-relaxed md:text-lg text-mauve-light">
         Reach out and let&apos;s create a meaningful journey together. At Soul
         Journey, we&apos;re here to support you on your path to self-discovery
         and growth.
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,16 +4,21 @@ import ContactImage from "../components/Contact/ContactImage";
 
 const Contact = () => {
   return (
-    <div>
-      <ContactHeader />
-      <div className="flex flex-col items-center justify-between px-4 py-32 md:flex-row">
-        {/* Contact Form */}
-        <div className="w-full mb-4 md:w-1/2 lg:w-1/2 md:pr-4 md:mb-0">
-          <ContactForm />
-        </div>
-        {/* Contact Image */}
-        <div className="w-full md:w-1/2 lg:w-1/2 md:pl-4">
-          <ContactImage />
+    <div className="py-36 contact-background">
+      <div className="px-4 mx-auto max-w-7xl">
+        {/* Contact Header */}
+        <ContactHeader />
+
+        {/* Contact Content */}
+        <div className="flex flex-col items-center justify-between px-4 py-16 bg-gray-100 bg-opacity-50 rounded-md md:flex-row">
+          {/* Contact Form */}
+          <div className="w-full mb-4 md:w-1/2 lg:w-1/2 md:pr-4 md:mb-0">
+            <ContactForm />
+          </div>
+          {/* Contact Image */}
+          <div className="w-full md:w-1/2 lg:w-1/2 md:pl-4">
+            <ContactImage />
+          </div>
         </div>
       </div>
     </div>
