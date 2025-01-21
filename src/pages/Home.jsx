1f094667fee5e654d5163fd59ff96ce43c19ffe5
Mustafa-Uslu <mustafauslu.de@gmail.com>
2025-01-21 17:24:17 +0100

    feat: Added FAQ component to Home page and updated styles in index.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "../components/home/HeroSection";
 import ProcessSection from "../components/home/ProcessSection";
+import FAQ from "../components/home/FAQ"
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <ProcessSection/>
+      <FAQ/>
     </>
   );
 };
