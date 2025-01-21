4f8e4b2e3d1f7f842bd8c0204ab09822decc38b4
ucangun <149247682+ucangun@users.noreply.github.com>
2025-01-21 22:39:59 +0100

    feat: updated Contact page with new ContactInfo component and styling changes

--- src/components/Contact/ContactHeader.jsx
+++ src/components/Contact/ContactHeader.jsx
@@ -4,7 +4,7 @@ const ContactHeader = () => {
       <h1 className="mb-4 text-3xl font-bold md:text-5xl text-pink-dark">
         Get in Touch
       </h1>
-      <p className="max-w-2xl mx-auto text-base leading-relaxed md:text-lg text-mauve-light">
+      <p className="max-w-2xl mx-auto text-base leading-relaxed md:text-lg text-offWhite">
         Reach out and let&apos;s create a meaningful journey together. At Soul
         Journey, we&apos;re here to support you on your path to self-discovery
         and growth.
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import ContactForm from "../components/Contact/ContactForm";
 import ContactHeader from "../components/Contact/ContactHeader";
 import ContactImage from "../components/Contact/ContactImage";
+import ContactInfo from "../components/Contact/ContactInfo";
 
 const Contact = () => {
   return (
@@ -10,15 +11,19 @@ const Contact = () => {
         <ContactHeader />
 
         {/* Contact Content */}
-        <div className="flex flex-col items-center justify-between px-4 py-16 bg-gray-100 bg-opacity-50 rounded-md md:flex-row">
+        <div className="flex flex-col items-center justify-between px-4 py-8 bg-gray-100 bg-opacity-50 rounded-md md:py-16 md:flex-row">
           {/* Contact Form */}
           <div className="w-full mb-4 md:w-1/2 lg:w-1/2 md:pr-4 md:mb-0">
             <ContactForm />
           </div>
           {/* Contact Image */}
-          <div className="w-full md:w-1/2 lg:w-1/2 md:pl-4">
+          <div className="hidden w-full md:block md:w-1/2 lg:w-1/2 md:pl-4">
             <ContactImage />
           </div>
+        </div>
+        {/* Contact Info */}
+        <div className="flex items-center justify-center py-8">
+          <ContactInfo />
         </div>
       </div>
     </div>
