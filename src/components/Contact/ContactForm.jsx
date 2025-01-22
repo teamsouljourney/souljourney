ee16ddba75a5d4b5170e09ccfbd43201d72474f7
ucangun <149247682+ucangun@users.noreply.github.com>
2025-01-22 01:21:01 +0100

    feat: Added emailjs functionality to contact form

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,6 +1,36 @@
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { toastErrorNotify, toastSuccessNotify } from "../../helper/ToastNotify";
+
 const ContactForm = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm(
+          import.meta.env.VITE_EMAILJS_SERVICE_ID,
+          import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+          form.current,
+          import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        )
+        .then(
+          () => {
+            toastSuccessNotify("Message sent successfully!");
+            form.current?.reset();
+          },
+          (error) => {
+            toastErrorNotify("Failed to send message!");
+            console.log(error.text);
+          }
+        );
+    }
+  };
+
   return (
-    <form id="contactForm">
+    <form ref={form} onSubmit={sendEmail}>
       <div className="mb-6 text-center">
         <h2 className="mb-2 text-2xl font-bold text-mauve-dark">Get Started</h2>
         <p className="text-sm font-semibold text-customBlack-dark dark:text-gray-300">
@@ -18,11 +48,11 @@ const ContactForm = () => {
             ></label>
             <input
               type="text"
-              id="name"
+              id="user_name"
               autoComplete="given-name"
               placeholder="Your name"
               className="w-full py-2 pl-2 pr-4 mb-2 border border-gray-400 rounded-md shadow-md dark:text-gray-300 sm:mb-0 focus:outline-none focus:ring focus:ring-mauve-light "
-              name="name"
+              name="user_name"
             />
           </div>
           <div className="mx-0 mb-1 sm:mb-4">
@@ -32,32 +62,33 @@ const ContactForm = () => {
             ></label>
             <input
               type="email"
-              id="email"
+              id="user_email"
               autoComplete="email"
               placeholder="Your email address"
               className="w-full py-2 pl-2 pr-4 mb-2 border border-gray-400 rounded-md shadow-md dark:text-gray-300 sm:mb-0 focus:outline-none focus:ring focus:ring-mauve-light"
-              name="email"
+              name="user_email"
             />
           </div>
         </div>
         <div className="mx-0 mb-1 sm:mb-4">
           <label
-            htmlFor="textarea"
+            htmlFor="message"
             className="pb-1 text-xs tracking-wider uppercase"
           ></label>
           <textarea
-            id="textarea"
-            name="textarea"
+            id="message"
             cols="30"
             rows="5"
             placeholder="Write your message..."
             className="w-full h-[8rem] py-2 pl-2 pr-4 mb-2 border border-gray-400 rounded-md shadow-md dark:text-gray-300 sm:mb-0  focus:outline-none focus:ring focus:ring-mauve-light"
+            name="message"
           ></textarea>
         </div>
       </div>
       <div className="text-center">
         <button
           type="submit"
+          value="Send"
           className="w-full px-6 py-3 text-white transition-all duration-200 rounded-md bg-mauve hover:bg-mauve-dark font-xl sm:mb-0"
         >
           Send Message
