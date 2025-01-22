d7caeb59fa307e1a94203869b84cee959b21b6c7
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-01-22 19:09:21 +0300

    feat: Added CSS styles for login and register pages and updated JSX files to use Material UI components and background images.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,33 @@
+import { Container } from "@mui/material"
+import Grid from '@mui/material/Grid2'
+import bgImage from '../assets/images/computer-4484282_1280.jpg'
 
 const Login = () => {
   return (
-    <div>
-      Login
-    </div>
+    <Container
+      maxWidth="xl"
+      sx={{
+        height: "100vh",
+        p: 0,
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat"
+      }}
+    >
+      <Grid
+        container
+        justifyContent="center"
+        direction="row-reverse"
+        rowSpacing={{sm: 3}}
+        sx={{
+          height: "100%",
+          p:2
+        }}
+      >
+        Register
+      </Grid>
+    </Container>
   )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { Container } from '@mui/material'
+import Grid from '@mui/material/Grid2'
+import bgImage from '../assets/images/weSupport.jpg'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <Container
+      maxWidth="xl"
+      sx={{
+        height: "100vh",
+        p: 0,
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat"
+      }}
+    >
+      <Grid
+        container
+        justifyContent="center"
+        direction="row-reverse"
+        rowSpacing={{sm: 3}}
+        sx={{
+          height: "100%",
+          p:2
+        }}
+      >
+        Register
+      </Grid>
+    </Container>
   )
 }
 
