53c9b485e369f6451ae34f4e62ede15f1800167e
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-01-22 20:18:45 +0300

    feat: added LoginHeader and RegisterHeader to components folder

---
+++ src/components/auth/LoginHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginHeader = () => {
+  return (
+    <div>LoginHeader</div>
+  )
+}
+
+export default LoginHeader
---
+++ src/components/auth/RegisterHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RegisterHeader = () => {
+  return (
+    <div>RegisterHeader</div>
+  )
+}
+
+export default RegisterHeader
