3225ca57e34efb117de44cd6f7a6ac357dea2645
Ayca96 <aycakubra1@gmail.com>
2025-01-23 12:11:42 +0100

    refactor:modified Card Component

--- src/components/home/BlogsSection.jsx
+++ src/components/home/BlogsSection.jsx
@@ -3,9 +3,9 @@ import Card from "./Card";
 const BlogsSection = () => {
   return (
     <>
-      <div className="relative flex justify-center items-center h-screen bg-offWhite overflow-hidden">
+      <div className="relative flex justify-center items-center h-screen overflow-hidden">
         {/* Background Animation */}
-        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-offWhite to-offWhite-dark animate-gradient-x opacity-30 blur-3xl "></div>
+        <div className="absolute top-0 left-0 w-full h-full  "></div>
 
         {/* Card Component */}
        <Card/>
