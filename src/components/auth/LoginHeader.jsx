cc81ec56fea9e2896f537e233b0a8624b1a960e2
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-01-23 16:34:33 +0300

    feat: Update typography styles in LoginHeader and RegisterHeader components

--- src/components/auth/LoginHeader.jsx
+++ src/components/auth/LoginHeader.jsx
@@ -4,7 +4,7 @@ import Grid from '@mui/material/Grid2'
 const LoginHeader = () => {
   return (
     <Grid item xs={12} mb={3}>
-      <Typography variant="h4" color="secondary" align="center">
+      <Typography variant="h4" color="secondary" align="center" sx={{fontWeight: "600"}}>
         {/* Welcome Back to Soul Journey
         Log in to Access Your Personal Path */}
         Ready to Continue Your Journey?
--- src/components/auth/RegisterHeader.jsx
+++ src/components/auth/RegisterHeader.jsx
@@ -4,7 +4,7 @@ import Grid from '@mui/material/Grid2'
 const RegisterHeader = () => {
   return (
     <Grid item xs={12} mb={3}>
-      <Typography variant="h4" color="secondary" align="center">
+      <Typography variant="h4" color="secondary" align="center" sx={{fontWeight: "600"}}>
         {/* Join Us on a Transformative Journey
         Sign Up and Begin Your Soul Journey*/}
         Create Your Account and Start Healing
