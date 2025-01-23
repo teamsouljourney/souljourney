e396e21bce765269ecfdf3ac4575d800f4ba83dc
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-01-23 13:39:16 +0300

    feat: Added Avatar and Typography components to Register page, updated Grid and Container styles.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,13 @@
-import { Container } from '@mui/material'
+import { Avatar, Container, Typography } from '@mui/material'
 import Grid from '@mui/material/Grid2'
 import bgImage from '../assets/images/weSupport.jpg'
 import RegisterHeader from '../components/auth/RegisterHeader'
+import LockIcon from "@mui/icons-material/Lock";
 
 const Register = () => {
   return (
     <Container
-      maxWidth="xl"
+      maxWidth="xxl"
       sx={{
         height: "100vh",
         p: 0,
@@ -26,7 +27,32 @@ const Register = () => {
           p:2
         }}
       >
-        <RegisterHeader/>
+        <Grid item xs={12} sm={6} md={4} lg={3}
+          sx={{
+            backgroundColor: "white",
+            p: 2,
+            borderRadius: "30px",
+            height: "700px",
+            width: "700px",
+            opacity: 0.5,
+            mt: 5
+          }}
+        >
+
+          <RegisterHeader/>
+          <Avatar
+            sx={{
+              m: "auto",
+              width: 40,
+              height: 40,
+            }}
+          >
+            <LockIcon size="30"/>
+          </Avatar>
+          <Typography textAlign="center">Sign Up</Typography>
+
+          
+        </Grid>
       </Grid>
     </Container>
   )
