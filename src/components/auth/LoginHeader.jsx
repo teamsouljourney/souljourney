95a22a76f77f53c78cdea91f3109302a0714a045
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-01-23 14:40:07 +0300

    feat: Update LoginHeader component to use Grid and Typography components with custom styles

--- src/components/auth/LoginHeader.jsx
+++ src/components/auth/LoginHeader.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const LoginHeader = () => {
   return (
-    <div>LoginHeader</div>
+    <Grid item xs={12} mb={3}>
+      <Typography variant="h3" align="center" 
+        sx={{color: "customColors.pink"}}
+      >
+        {/* Welcome Back to Soul Journey
+        Log in to Access Your Personal Path */}
+        Ready to Continue Your Journey?
+      </Typography>
+    </Grid>
   )
 }
 
