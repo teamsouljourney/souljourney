d05e37f7057d2690a86b53ea3d80a1e4b98e31b1
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-01-27 00:19:42 +0300

    feat: Added axios instance creation and import in useAxios hook

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,4 +1,9 @@
+import axios from 'axios'
 import React from 'react'
+
+export const axiosPublic = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL
+})
 
 const useAxios = () => {
   return (
