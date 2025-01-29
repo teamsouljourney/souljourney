0cb7022cb9057722ee07cc7003ce94173b15f890
Ayca96 <aycakubra1@gmail.com>
2025-01-29 16:05:00 +0100

    refactor:modified home/BlogsSections,team/Card,team.json

--- src/components/home/BlogsSection.jsx
+++ src/components/home/BlogsSection.jsx
@@ -1,14 +1,24 @@
-import teams from "../../helper/team.json"
+import teams from "../../helper/team.json";
 import Card from "./Card";
 const BlogsSection = () => {
   return (
     <>
-      <div className="relative flex justify-center items-center h-screen overflow-hidden">
-        {/* Background Animation */}
-        <div className="absolute top-0 left-0 w-full h-full  "></div>
-
+      <div className=" flex flex-col items-center gap-10 w-full">
+        <div className="text-center  font-urbanist ">
+          <h1 className="mb-7">Our Blogs</h1>
+          <p className="w-[60%] mx-auto font-urbanist">
+            Our blog is a valuable resource filled with insightful articles on
+            psychological counseling, personal development, and mental health.
+            Written by our team of experienced psychologists, therapists, and
+            counselors, our articles aim to help you better understand yourself
+            and cultivate a healthier mindset. Here’s what you can expect from
+            our blog.
+          </p>
+        </div>
         {/* Card Component */}
-       <Card teams={teams}/>
+        <div className="flex flex-wrap justify-center items-center">
+          <Card teams={teams} />
+        </div>
       </div>
     </>
   );
