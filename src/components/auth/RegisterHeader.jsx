b98017f91e0d3d1aa75c4b2f835f459ae4011126
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-02-01 16:10:05 +0300

    refactor: Update color of Typography in LoginHeader and RegisterHeader components to "primary" from "customColors.pink"

--- src/components/auth/RegisterHeader.jsx
+++ src/components/auth/RegisterHeader.jsx
@@ -6,7 +6,7 @@ const RegisterHeader = () => {
     <Grid xs={12} mb={1}>
       <Typography
         variant="h4"
-        color="customColors.pink"
+        color="primary"
         align="center"
         sx={{
           fontWeight: "600",
