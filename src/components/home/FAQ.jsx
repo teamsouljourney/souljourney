df39121339df67e4d1c8060954515f64cfe8c983
Mustafa-Uslu <mustafauslu.de@gmail.com>
2025-02-03 17:36:53 +0100

    update: Refactor FAQ component: add question toggling, update styles and layout`

--- src/components/home/FAQ.jsx
+++ src/components/home/FAQ.jsx
@@ -5,7 +5,11 @@ import "../../index.css";
 
 const FAQ = () => {
     const [visibleQuestions, setVisibleQuestions] = useState(5);
+    const [openIndex, setOpenIndex] = useState(null);
 
+    const toggleQuestion = (index) => {
+        setOpenIndex(openIndex === index ? null : index);
+    };
     const showMoreQuestions = () => {
       setVisibleQuestions((prev) => Math.min(prev + 5, faqData.length));
     };
@@ -16,9 +20,9 @@ const FAQ = () => {
     <>
       <div className="relative isolate overflow-hidden bg-custom">
         <div className="py-24 px-8 max-w-5xl mx-auto flex flex-col md:flex-row gap-12">
-          <div className="flex flex-col text-left basis-1/2">
+          <div className="flex flex-col text-center basis-1/2">
             <p className="inline-block font-semibold text-primary mb-4">F.A.Q</p>
-            <p className="sm:text-4xl text-3xl font-extrabold text-base-content">
+            <p className="sm:text-4xl text-3xl font-extrabold text-base-content font-[Urbanist] text-center">
               Frequently Asked Questions
             </p>
             <div
@@ -35,12 +39,13 @@ const FAQ = () => {
                   key={index}
                 >
                   <button
-                    className="relative flex gap-2 items-center w-full py-5 text-base font-semibold text-left border-t md:text-lg border-base-content/10"
+                    className="relative flex gap-2 items-center w-full py-5 text-base font-semibold text-left border-t md:text-lg border-base-content/10 bg-navy-200 hover:bg-navy-300 transition-colors duration-300 rounded-md px-4"
                     aria-expanded="false"
+                    onClick={() => toggleQuestion(index)}
                   >
                     <span className="flex-1 text-base-content">{faq.question}</span>
                     <svg
-                      className="flex-shrink-0 w-4 h-4 ml-auto fill-current"
+                     className={`flex-shrink-0 w-4 h-4 ml-auto fill-current transition-transform duration-300 ${openIndex === index ? "rotate-180" : "rotate-0"}`}
                       viewBox="0 0 16 16"
                       xmlns="http://www.w3.org/2000/svg"
                     >
@@ -49,22 +54,22 @@ const FAQ = () => {
                         width="16"
                         height="2"
                         rx="1"
-                        className="transform origin-center transition duration-200 ease-out"
+                        className="transform origin-center transition duration-200"
                       ></rect>
                       <rect
                         y="7"
                         width="16"
                         height="2"
                         rx="1"
-                        className="group-hover:opacity-0 transform origin-center rotate-90 transition duration-200 ease-out"
+                        className="group-hover:opacity-0 transform origin-center rotate-90 transition duration-100 ease-out"
                       ></rect>
                     </svg>
                   </button>
                   <div
-                    className="transition-all duration-300 ease-in-out group-hover:max-h-60 max-h-0 overflow-hidden"
-                    style={{ transition: "max-height 0.3s ease-in-out 0s" }}
+                    className="transition-all duration-500 ease-in-out group-hover:max-h-60 max-h-0 overflow-hidden"
+                    style={{ transition: "max-height 0.5s ease-in-out 0s" }}
                   >
-                    <div className="pb-5 leading-relaxed">
+                    <div className="pb-5 leading-relaxed px-4">
                       <div className="space-y-2 leading-relaxed">{faq.answer}</div>
                     </div>
                   </div>
@@ -75,7 +80,7 @@ const FAQ = () => {
               <div className="text-center mt-4">
                 <button
                   onClick={visibleQuestions < faqData.length ? showMoreQuestions : showLessQuestions}
-                  className="px-4 py-2 bg-orange-500 text-white font-bold rounded-md hover:bg-orange-600"
+                  className="px-4 py-2 bg-[hsl(164,51%,69%)] text-black font-bold rounded-full hover:bg-[#67C3A8] transition-all duration-300"
                 >
                    {visibleQuestions < faqData.length ? "See More  →" : "See Less  →"}
                 </button>
