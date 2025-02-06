c2abe56b18c28b86fb85bf24e5fbbb1ef5564796
ucangun <149247682+ucangun@users.noreply.github.com>
2025-02-06 13:43:37 +0100

    refactor: Refactored TeamDetail component to use useTherapistCall hook and useSelector from react-redux

--- src/components/team/TeamDetail.jsx
+++ src/components/team/TeamDetail.jsx
@@ -1,64 +1,69 @@
+import { useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import teams from "../../helper/team.json";  // Takım verilerinizi içeren JSON dosyası
+import useTherapistCall from "../../hooks/useTherapistCall";
+import { useSelector } from "react-redux";
 
 const TeamDetail = () => {
-  const { id } = useParams(); 
-  const navigate = useNavigate(); // Yönlendirme için useNavigate kullanıyoruz
-  const team = teams.find((team) => team.id === parseInt(id)); 
-  
-  if (!team) {
-    return (
-      <div className="text-center text-red-500">
-        <h1>Team not found!</h1>
-      </div>
-    );
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { getSingleTherapist } = useTherapistCall();
+  const { singleTherapist, loading, error } = useSelector(
+    (state) => state.therapists
+  );
+
+  useEffect(() => {
+    getSingleTherapist(id);
+  }, [id]);
+
+  if (loading) {
+    return <div className="text-center text-navy">Loading...</div>;
+  }
+
+  if (error || !singleTherapist) {
+    return <div className="text-center text-mauve">Therapist not found!</div>;
   }
 
   return (
-    <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16 bg-offWhite">
-      <div className="rounded overflow-hidden flex flex-col max-w-xl mx-auto">
+    <div className="max-w-screen-xl p-5 mx-auto sm:p-10 md:p-16 bg-offWhite">
+      <div className="flex flex-col max-w-xl mx-auto overflow-hidden rounded">
         <a href="#">
-          {/* Takımın resmini dinamik olarak yüklüyoruz */}
-          <img className="w-full" src="https://media.istockphoto.com/id/1346944001/de/foto/nahaufnahme-von-kollegen-die-ihre-h%C3%A4nde-stapeln.jpg?s=612x612&w=0&k=20&c=nz6p6DhsiCWg5H8R7UlnGmAVxpFjNMCwfuqIRI6k0-o=" alt={team.name} />
+          <img
+            className="w-full"
+            src={singleTherapist.image}
+            alt={singleTherapist.name}
+          />
         </a>
-        <div className="relative -mt-16 px-10 pt-5 pb-16 bg-white m-10">
+        <div className="relative px-10 pt-5 pb-16 m-10 -mt-16 bg-white">
           <a
             href="#"
-            className="font-semibold text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2"
+            className="inline-block mb-2 text-lg font-semibold transition duration-500 ease-in-out hover:text-indigo-600"
           >
-            {/* Takım adını dinamik olarak yüklüyoruz */}
-            {team.name}
+            {singleTherapist.name}
           </a>
-          <p className="text-gray-500 text-sm">
-            {/* Takım açıklamasını dinamik olarak yüklüyoruz */}
-            {team.description}
-          </p>
-          <p className="mt-5 text-gray-600 text-xs">
-            {/* Takım alanı bilgisi */}
+          <p className="text-sm text-gray-500">{singleTherapist.description}</p>
+          <p className="mt-5 text-xs text-gray-600">
             By{" "}
             <a
               href="#"
               className="text-xs text-indigo-600 transition duration-500 ease-in-out"
             >
-              {team.name}
+              {singleTherapist.name}
             </a>{" "}
             | in{" "}
             <a
               href="#"
               className="text-xs text-indigo-600 transition duration-500 ease-in-out"
             >
-              {team.area}
+              {singleTherapist.name}
             </a>
           </p>
         </div>
       </div>
 
-      {/* Sayfanın en altında Geri Butonu */}
-      <div className="text-center mt-8 p-6 pt-0">
-        <button 
-          onClick={() => navigate(-1)}  // Geri gitmek için -1 kullanıyoruz
-          className="bg-gradient-to-r from-navy to-seaGreen text-white py-3 px-6 rounded-lg hover:bg-gradient-to-r hover:from-navy-dark hover:to-seaGreen-dark text-lg"
-  
+      <div className="p-6 pt-0 mt-8 text-center">
+        <button
+          onClick={() => navigate(-1)}
+          className="px-6 py-3 text-lg text-white rounded-lg bg-gradient-to-r from-navy to-seaGreen hover:bg-gradient-to-r hover:from-navy-dark hover:to-seaGreen-dark"
         >
           Go Back
         </button>
