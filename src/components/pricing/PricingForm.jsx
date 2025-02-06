2f2dc8e91e046c9cfb8308a189656869264c240d
fatihua <fatihusameavseren@gmail.com>
2025-02-06 11:49:15 +0100

    feat:Add new arrangements to the Pricing Page.

--- src/components/pricing/PricingForm.jsx
+++ src/components/pricing/PricingForm.jsx
@@ -4,15 +4,13 @@ const PricingForm = () => {
 
     const [selectedPlan, setSelectedPlan] = useState("Monthly");
 
-    
-
   return (
     <div>
-    <section className="bg-white dark:bg-gray-900">
+    <section className="dark:bg-gray-900 bg-offWhite">
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div className="mx-auto max-w-screen-md text-center mb-4 lg:mb-6">
-          <h2 className="mb-4 text-center text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Plans & Pricing</h2>
-          <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+          <h2 className="mb-4 text-center text-3xl md:text-4xl tracking-tight  text-navy dark:text-white">Plans & Pricing</h2>
+          <p className="mb-5 font-light text-navy-light gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
       </div>
       <div className="flex flex-col sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4 mb-4">
         <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
@@ -28,7 +26,7 @@ const PricingForm = () => {
 
       <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
           {/* <!-- Pricing Card --> */}
-          <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-[#f4f3f3]  rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
+          <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-navy bg-white  rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
               <h3 className="mb-4 text-2xl font-semibold">Free</h3>
               <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">Best option for personal use & for your next project.</p>
               <div className="flex justify-center items-baseline my-8">
@@ -53,12 +51,11 @@ const PricingForm = () => {
                       <span>Team size: <span className="font-semibold">1 developer</span></span>
                   </li>
                   
-                  
               </ul>
-              <a href="#" className="bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center  dark:focus:ring-primary-900 hover:bg-[#cbc5cb] dark:hover:bg-white dark:hover:text-black dark:text-white">Get started</a>
+              <a href="#" className="bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center  dark:focus:ring-primary-900 hover:bg-offWhite-dark white dark:hover:bg-white dark:hover:text-black dark:text-white">Get started</a>
           </div>
           {/* <!-- Pricing Card --> */}
-          <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-[#f4f3f3] rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
+          <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-navy bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
               <h3 className="mb-4 text-2xl font-semibold">Company</h3>
               <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">Relevant for multiple users, extended & premium support.</p>
               <div className="flex justify-center items-baseline my-8">
@@ -85,10 +82,10 @@ const PricingForm = () => {
                   
                   
               </ul>
-              <a href="#" className="bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 hover:bg-[#cbc5cb] dark:hover:text-black dark:hover:bg-white">Get started</a>
+              <a href="#" className="bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 hover:bg-offWhite-dark dark:hover:text-black dark:hover:bg-white">Get started</a>
           </div>
           {/* <!-- Pricing Card --> */}
-          <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-white bg-[#1e5ea8] rounded-lg border border-gray-100 shadow-blue-700 dark:border-gray-600 xl:p-8 dark:bg-gray-700 dark:text-white">
+          <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-white bg-navy rounded-lg border border-gray-100 shadow-blue-700 dark:border-gray-600 xl:p-8 dark:bg-gray-700 dark:text-white">
               <h3 className="text-white mb-4 text-2xl font-semibold">Enterprise</h3>
               <p className="font-light sm:text-lg text-white">Best for large scale uses and extended redistribution rights.</p>
               <div className="flex justify-center items-baseline my-8">
@@ -115,7 +112,7 @@ const PricingForm = () => {
 
                  
               </ul>
-              <a href="#" className="text-white  bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-base px-5 py-2.5 text-center dark:text-white dark:hover:text-black dark:focus:ring-primary-900 hover:bg-[#8F5B8A] dark:hover:bg-[#c3bac2]">Get started</a>
+              <a href="#" className="text-white  bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-base px-5 py-2.5 text-center dark:text-white dark:hover:text-black dark:focus:ring-primary-900 hover:bg-seaGreen dark:hover:bg-[#c3bac2]">Get started</a>
           </div>
       </div>
   </div>
