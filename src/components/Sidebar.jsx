a7afa28076f48ad0a03ae7bdbf08abe444594cf8
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-02-07 12:43:40 +0300

    refactor: Update Sidebar component: replace a tags with NavLink components`

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -155,12 +155,12 @@ const Sidebar = () => {
 
   return (
     <>
-      <div className=" w-dvw h-dvh bg-red-200 grid grid-cols-7">
+      <div className=" w-dvw h-dvh grid grid-cols-7">
         {/* SideBar */}
         <div className="col-span-1 bg-white">
-          <div className="p-2 h-full w-full flex flex-col bg-white dark:bg-navy border-r border-r-gray-200">
+          <div className="p-2 h-full w-full flex flex-col bg-offWhite dark:bg-navy border-r border-r-gray-200">
             {/* Logo */}
-            <a href="#">
+            <NavLink to="#">
               <div className="flex justify-center lg:justify-start items-center gap-2 py-2 px-0 md:px-2 lg:px-4 cursor-pointer ">
                 <div className="flex items-center">
                   <NavLink to="/">
@@ -172,7 +172,7 @@ const Sidebar = () => {
                   </NavLink>
                 </div>
               </div>
-            </a>
+            </NavLink>
             <div className="flex flex-col h-full overflow-y-auto overflow-x-hidden flex-grow pt-2 justify-between">
               <div className="flex flex-col  space-y-1 mx-1 lg:mt-1 ">
                 <div className="px-5 pt-4 hidden lg:block">
@@ -182,9 +182,35 @@ const Sidebar = () => {
                     </div>
                   </div>
                 </div>
-                <a
+                <NavLink
                   className="flex flex-row items-center  justify-center lg:justify-start rounded-md h-12 focus:outline-none pr-3.5  lg:pr-6 font-semibold text-gray-500 hover:text-primary-400 cursor-pointer "
-                  href="/app"
+                  to=""
+                >
+                  <span className="inline-flex justify-center items-center ml-3.5">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1.25rem"
+                      height="1.25rem"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M10.894 22h2.212c3.447 0 5.17 0 6.345-1.012s1.419-2.705 1.906-6.093l.279-1.937c.38-2.637.57-3.956.029-5.083s-1.691-1.813-3.992-3.183l-1.385-.825C14.2 2.622 13.154 2 12 2s-2.199.622-4.288 1.867l-1.385.825c-2.3 1.37-3.451 2.056-3.992 3.183s-.35 2.446.03 5.083l.278 1.937c.487 3.388.731 5.081 1.906 6.093S7.447 22 10.894 22"
+                        opacity=".5"
+                      />
+                      <path
+                        fill="currentColor"
+                        d="M9.447 15.397a.75.75 0 0 0-.894 1.205A5.77 5.77 0 0 0 12 17.75a5.77 5.77 0 0 0 3.447-1.148a.75.75 0 0 0-.894-1.205A4.27 4.27 0 0 1 12 16.25a4.27 4.27 0 0 1-2.553-.853"
+                      />
+                    </svg>
+                  </span>
+                  <span className="ml-0 lg:ml-2 text-sm tracking-wide truncate capitalize hidden lg:block">
+                    Dashboard
+                  </span>
+                </NavLink>
+                <NavLink
+                  className="flex flex-row items-center  justify-center lg:justify-start rounded-md h-12 focus:outline-none pr-3.5  lg:pr-6 font-semibold text-gray-500 hover:text-primary-400 cursor-pointer "
+                  to="/app"
                 >
                   <span className="inline-flex justify-center items-center ml-3.5">
                     <svg
@@ -207,10 +233,10 @@ const Sidebar = () => {
                   <span className="ml-0 lg:ml-2 text-sm tracking-wide truncate capitalize hidden lg:block">
                     Appointments
                   </span>
-                </a>
-                <a
+                </NavLink>
+                <NavLink
                   className="flex flex-row items-center  justify-center lg:justify-start rounded-md h-12 focus:outline-none pr-3.5  lg:pr-6 font-semibold text-gray-500 hover:text-primary-400 cursor-pointer "
-                  href="/app/blogs"
+                  to="/private/write-blog"
                 >
                   <span className="inline-flex justify-center items-center ml-3.5">
                     <svg
@@ -243,10 +269,10 @@ const Sidebar = () => {
                   <span className="ml-0 lg:ml-2 text-sm tracking-wide truncate capitalize hidden lg:block">
                     Write Blog
                   </span>
-                </a>
-                <a
+                </NavLink>
+                <NavLink
                   className="flex flex-row items-center  justify-center lg:justify-start rounded-md h-12 focus:outline-none pr-3.5  lg:pr-6 font-semibold text-gray-500 hover:text-primary-400 cursor-pointer "
-                  href="/app/clients"
+                  to="/app/clients"
                 >
                   <span className="inline-flex justify-center items-center ml-3.5">
                     <svg
@@ -273,10 +299,10 @@ const Sidebar = () => {
                   <span className="ml-0 lg:ml-2 text-sm tracking-wide truncate capitalize hidden lg:block">
                     Chat
                   </span>
-                </a>
-                <a
+                </NavLink>
+                <NavLink
                   className="flex flex-row items-center  justify-center lg:justify-start rounded-md h-12 focus:outline-none pr-3.5  lg:pr-6 font-semibold bg-primary-50 shadow-sm text-primary-400  "
-                  href="/private/video-call"
+                  to="/private/video-call"
                 >
                   <span className="inline-flex justify-center items-center ml-3.5">
                     <svg
@@ -305,7 +331,7 @@ const Sidebar = () => {
                   <span className="ml-0 lg:ml-2 text-sm tracking-wide truncate capitalize hidden lg:block">
                     Video Call
                   </span>
-                </a>
+                </NavLink>
               </div>
               <div className="flex flex-col  space-y-1 mx-1 lg:mt-1 ">
                 <a
