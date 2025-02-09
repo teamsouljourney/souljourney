391b3a2599d379c127ba4c0072ffb42752661b80
fatihua <fatihusameavseren@gmail.com>
2025-02-09 10:10:59 +0100

    feat: Add Private Router

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,7 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 
 const PrivateRouter = () => {
- return <Outlet/>
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const { auth } = useSelector((state) => state.auth);
+
+    const isAuthenticated = auth?.currentUser;
+
+    if (!isAuthenticated) {
+        navigate("/login", { state: { from: location } });
+        toastErrorNotify("You must be logged in to access this page.");
+        return null; 
+    }else{
+        toastSuccessNotify("You are logged in.");
+        return <Outlet/>
+    }
 };
 
 export default PrivateRouter;
