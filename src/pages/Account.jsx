7124a3b86884e829fdf501a73a507b52d0f688eb
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-02-13 14:37:24 +0300

    refactor: Updated Account page with password visibility toggle and new password fields

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,22 +1,23 @@
-import { Box, Container, Typography } from "@mui/material";
-import Grid from "@mui/material/Grid2";
-import { Formik } from "formik";
-import { SignupSchema } from "../components/auth/RegisterForm";
-import {
-  authMainContainerGridStyle,
-  authFormContainerGridStyle,
-} from "../styles/globalStyle";
-import AccountForm from "../components/auth/AccountForm";
+// import { Box, Container, Typography } from "@mui/material";
+// import Grid from "@mui/material/Grid2";
+// import { Formik } from "formik";
+// import { SignupSchema } from "../components/auth/RegisterForm";
+// import {
+//   authMainContainerGridStyle,
+//   authFormContainerGridStyle,
+// } from "../styles/globalStyle";
+// import AccountForm from "../components/auth/AccountForm";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const Account = () => {
   const { t } = useTranslation();
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <div className="container max-w-none flex grow shrink-0 basis-0 flex-col items-center gap-1 self-stretch bg-offWhite-light py-8 shadow-sm">
-
+    <div className="container max-w-none flex grow shrink-0 basis-0 flex-col items-center gap-4 self-stretch bg-offWhite-light py-8 shadow-sm text-navy-dark">
       {/* Profile */}
 
-      <div className="flex w-full max-w-[576px] flex-col items-start gap-6 border-b-2">
+      <div className="flex w-full max-w-[576px] flex-col items-start gap-10 border-b-2">
         <div className="flex w-full flex-col items-start gap-1">
           <span className="w-full text-heading-2 font-heading-2 text-default-font text-2xl">
             Account
@@ -39,13 +40,12 @@ const Account = () => {
                 src="https://res.cloudinary.com/subframe/image/upload/v1711417513/shared/kwut7rhuyivweg8tmyzl.jpg"
               />
               <div className="flex flex-col items-start gap-2">
-                {/* <Button
-              variant="neutral-secondary"
-              icon="FeatherUpload"
-              onClick={(event: React.MouseEvent<HTMLButtonElement>) => {}}
-            >
-              Upload
-                </Button> */}
+                <button
+                  type="button"
+                  class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50"
+                >
+                  Upload
+                </button>
                 <span className="text-caption font-caption text-subtext-color">
                   For best results, upload an image 512x512 or larger.
                 </span>
@@ -53,12 +53,10 @@ const Account = () => {
             </div>
           </div>
           <div className="flex w-full items-center gap-4">
+            {/* Input Field */}
             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6">
               <div class="sm:col-span-3">
-                <label
-                  for="firstName"
-                  class="block text-sm/6 font-medium text-gray-900"
-                >
+                <label for="firstName" class="block text-sm/6 font-medium">
                   First name
                 </label>
                 <div class="mt-2">
@@ -74,10 +72,7 @@ const Account = () => {
               </div>
 
               <div class="sm:col-span-3">
-                <label
-                  for="lastName"
-                  class="block text-sm/6 font-medium text-gray-900"
-                >
+                <label for="lastName" class="block text-sm/6 font-medium">
                   Last name
                 </label>
                 <div class="mt-2">
@@ -93,10 +88,7 @@ const Account = () => {
               </div>
 
               <div class="sm:col-span-4">
-                <label
-                  for="email"
-                  class="block text-sm/6 font-medium text-gray-900"
-                >
+                <label for="email" class="block text-sm/6 font-medium">
                   Email
                 </label>
                 <div class="mt-2">
@@ -113,10 +105,7 @@ const Account = () => {
 
               {/* Personel Info */}
               <div class="sm:col-span-4">
-                <label
-                  for="phone"
-                  class="block text-sm/6 font-medium text-gray-900"
-                >
+                <label for="phone" class="block text-sm/6 font-medium">
                   Phone
                 </label>
                 <div class="mt-2">
@@ -131,10 +120,7 @@ const Account = () => {
                 </div>
               </div>
               <div class="col-span-full">
-                <label
-                  for="profession"
-                  class="block text-sm/6 font-medium text-gray-900"
-                >
+                <label for="profession" class="block text-sm/6 font-medium">
                   Profession
                 </label>
                 <div class="mt-2">
@@ -149,10 +135,7 @@ const Account = () => {
                 </div>
               </div>
               <div class="col-span-full">
-                <label
-                  for="address"
-                  class="block text-sm/6 font-medium text-gray-900"
-                >
+                <label for="address" class="block text-sm/6 font-medium">
                   Address
                 </label>
                 <div class="mt-2">
@@ -162,135 +145,126 @@ const Account = () => {
                     id="address"
                     placeholder="Write your address..."
                     autocomplete="address"
-                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 mb-4"
                   />
                 </div>
               </div>
-
-              {/* <div class="col-span-full">
-                <label
-                  for="about"
-                  class="block text-sm/6 font-medium text-gray-900"
-                >
-                  About/feedback
-                </label>
-                <div class="mt-2">
-                  <textarea
-                    name="about"
-                    id="about"
-                    rows="3"
-                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  ></textarea>
-                </div>
-                <p class="mt-3 text-sm/6 text-gray-600">
-                  Write a few sentences about yourself.
-                </p>
-              </div> */}
-
             </div>
           </div>
         </div>
-        <div className="flex w-full items-center gap-4">
-          {/* <TextField
-          className="h-auto grow shrink-0 basis-0"
-          label="Email"
-          helpText=""
-        >
-          <TextField.Input
-            placeholder="josef@subframe.com"
-            value=""
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {}}
-          />
-          </TextField> */}
-        </div>
+        
       </div>
-      <div className="flex h-px w-full flex-none flex-col items-center gap-2 bg-neutral-border" />
+      <div className="flex h-px w-full flex-none flex-col items-center bg-neutral-border" />
 
       {/* Password */}
-      
+
       <div className="flex w-full max-w-[576px] flex-col items-start gap-4 border-b-2">
         <span className="text-heading-3 font-heading-3 text-default-font text-lg">
           Password
         </span>
 
-        <div class="sm:col-span-4 ">
-          <label for="password" class="block text-sm/6 font-medium text-gray-900">
+        <div className="sm:col-span-4 relative">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-900"
+          >
             Current Password
           </label>
-          <div class="mt-2">
+          <div className="mt-2 relative">
             <input
               id="password"
               name="password"
-              type="password"
-              placeholder="Write your current password..."
-              autocomplete="password"
-              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter current password"
+              autoComplete="current-password"
+              className="block w-full min-w-[280px] rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-xs placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
             />
-          </div>
-        </div>
-        <div class="sm:col-span-4 ">
-          <label for="new-password" class="block text-sm/6 font-medium text-gray-900">
-            New Password
-          </label>
-          <div class="mt-2">
-            <input
-              id="new-password"
-              name="new-password"
-              type="password"
-              placeholder="Write your new password..."
-              autocomplete="password"
-              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-            />
-          </div>
-        </div>
-        <div class="sm:col-span-4 ">
-          <label for="retype-password" class="block text-sm/6 font-medium text-gray-900">
-            New Password
-          </label>
-          <div class="mt-2">
-            <input
-              id="retype-password"
-              name="retype-password"
-              type="password"
-              placeholder="Re-write your new password..."
-              autocomplete="password"
-              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute inset-y-0 right-2 flex items-center px-2 text-gray-500 hover:text-gray-700"
+            >
+              <img
+                src={
+                  showPassword
+                    ? "/assets/visible1.svg"
+                    : "/assets/invisible1.svg"
+                }
+                alt={showPassword ? "Hide password" : "Show password"}
+                className="w-4 h-4  opacity-60 hover:opacity-100 transition-opacity"
+              />
+            </button>
           </div>
         </div>
 
-        {/* <TextField
-        className="h-auto w-full flex-none"
-        label="Current password"
-        helpText=""
-      >
-        <TextField.Input
-          type="password"
-          placeholder="Enter current password"
-          value=""
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {}}
-        />
-      </TextField>
-      <TextField
-        className="h-auto w-full flex-none"
-        label="New password"
-        helpText="Your password must have at least 8 characters, include one uppercase letter, and one number."
-      >
-        <TextField.Input
-          type="password"
-          placeholder="Enter new password"
-          value=""
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {}}
-        />
-      </TextField>
-      <TextField className="h-auto w-full flex-none" label="" helpText="">
-        <TextField.Input
-          type="password"
-          placeholder="Re-type new password"
-          value=""
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {}}
-        />
-      </TextField> */}
+        <div className="sm:col-span-4 relative">
+          <label
+            htmlFor="new-password"
+            className="block text-sm font-medium text-gray-900"
+          >
+            New Password
+          </label>
+          <div className="mt-2 relative">
+            <input
+              id="new-password"
+              name="new-password"
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter new password"
+              autoComplete="current-password"
+              className="block w-full min-w-[280px] rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-xs placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute inset-y-0 right-2 flex items-center px-2 text-gray-500 hover:text-gray-700"
+            >
+              <img
+                src={
+                  showPassword
+                    ? "/assets/visible1.svg"
+                    : "/assets/invisible1.svg"
+                }
+                alt={showPassword ? "Hide password" : "Show password"}
+                className="w-4 h-4  opacity-60 hover:opacity-100 transition-opacity"
+              />
+            </button>
+          </div>
+        </div>
+
+        <div className="sm:col-span-4 relative">
+          <label
+            htmlFor="retype-password"
+            className="block text-sm font-medium text-gray-900"
+          >
+            Re-type New Password
+          </label>
+          <div className="mt-2 relative">
+            <input
+              id="retype-password"
+              name="retype-password"
+              type={showPassword ? "text" : "password"}
+              placeholder="Re-type new password"
+              autoComplete="retype-password"
+              className="block w-full min-w-[280px] rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-xs placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute inset-y-0 right-2 flex items-center px-2 text-gray-500 hover:text-gray-700"
+            >
+              <img
+                src={
+                  showPassword
+                    ? "/assets/visible1.svg"
+                    : "/assets/invisible1.svg"
+                }
+                alt={showPassword ? "Hide password" : "Show password"}
+                className="w-4 h-4  opacity-60 hover:opacity-100 transition-opacity"
+              />
+            </button>
+          </div>
+        </div>
+
         <div className="flex w-full flex-col items-start justify-center gap-6">
           {/* <Button onClick={(event: React.MouseEvent<HTMLButtonElement>) => {}}>
           Change password
@@ -299,7 +273,7 @@ const Account = () => {
       </div>
 
       {/* ------- */}
-      <div className="flex h-px w-full flex-none flex-col items-center gap-2 bg-neutral-border " />
+      <div className="flex h-px w-full flex-none flex-col items-center bg-neutral-border " />
 
       {/* Delete Account */}
 
