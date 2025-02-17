5790802813873a414454db72695399eb9787ae32
Ayça Kübra Soruklu <aycakubra1@gmail.com>
2025-02-17 11:03:40 +0100

    refactor: modified components Card, MoreCategories, TeamCard,added:TabSwitch

---
+++ src/components/team/TeamTabSwitch.jsx
@@ -0,0 +1,13 @@
+import TabSwitch from "../../components/TabSwitch";
+
+const TeamTabSwitch = () => {
+  return (
+    <>
+      <div className="w-full h-full mt-5  mx-auto">
+        <TabSwitch />
+      </div>
+    </>
+  );
+};
+
+export default TeamTabSwitch;
