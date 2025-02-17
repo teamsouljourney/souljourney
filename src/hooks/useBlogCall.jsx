9f056099ae579554f6dfea2cb71cb64efff1a59d
ucangun <149247682+ucangun@users.noreply.github.com>
2025-02-17 10:33:05 +0100

    feat: Added createNewBlog and updateBlog functions to useBlogCall hook

--- src/hooks/useBlogCall.jsx
+++ src/hooks/useBlogCall.jsx
@@ -6,24 +6,22 @@ import {
   getAllBlogsSuccess,
   getSingleBlogSuccess,
 } from "../features/blogSlice";
-import { toastErrorNotify } from "../helper/ToastNotify";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 
 const useBlogCall = () => {
   const dispatch = useDispatch();
-//   const axiosWithToken = useAxios();
+  const axiosWithToken = useAxios();
 
   const getAllBlogs = async () => {
     dispatch(fetchStart());
     try {
       const { data } = await axiosPublic.get("blogs");
       console.log("APIden gelen veriler :", data.data);
-      
       dispatch(getAllBlogsSuccess(data.data));
     } catch (error) {
       dispatch(fetchFail());
       toastErrorNotify(
-        error.response.data.message,
-        "Failed to fetch blogs."
+        error.response?.data?.message || "Failed to fetch blogs."
       );
     }
   };
@@ -36,14 +34,42 @@ const useBlogCall = () => {
     } catch (error) {
       dispatch(fetchFail());
       toastErrorNotify(
-        error.response.data.message,
-        "Failed to fetch blogs details."
+        error.response?.data?.message || "Failed to fetch blog details."
       );
     }
   };
 
+  const createNewBlog = async (blogData) => {
+    dispatch(fetchStart());
+    try {
+      await axiosWithToken.post("blogs", blogData);
+      toastSuccessNotify("Blog created successfully!");
+    } catch (error) {
+      dispatch(fetchFail());
+      toastErrorNotify(
+        error.response?.data?.message || "Failed to create blog."
+      );
+    } finally {
+      getAllBlogs();
+    }
+  };
 
-  return { getAllBlogs, getSingleBlog};
+  const updateBlog = async (id, blogData) => {
+    dispatch(fetchStart());
+    try {
+      await axiosWithToken.put(`blogs/${id}`, blogData);
+      toastSuccessNotify("Blog updated successfully!");
+    } catch (error) {
+      dispatch(fetchFail());
+      toastErrorNotify(
+        error.response?.data?.message || "Failed to update blog."
+      );
+    } finally {
+      getAllBlogs();
+    }
+  };
+
+  return { getAllBlogs, getSingleBlog, createNewBlog, updateBlog };
 };
 
 export default useBlogCall;
