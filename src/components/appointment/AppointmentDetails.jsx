5b82a576702aa49c4ce0f207f7cc134201b58f4b
ucangun <149247682+ucangun@users.noreply.github.com>
2025-02-18 22:45:18 +0100

    feat:Added i18n support to appointment-related components

--- src/components/appointment/AppointmentDetails.jsx
+++ src/components/appointment/AppointmentDetails.jsx
@@ -1,25 +1,28 @@
 import { useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
 import { formatDateTime } from "../../helper/dateFormatter";
 
 const AppointmentDetails = () => {
+  const { t } = useTranslation();
   const { singleAppointment } = useSelector((state) => state.appointments);
 
   return (
     <div className="mt-4 space-y-2 text-gray-700">
       <p>
-        <strong>Client:</strong> {singleAppointment.userId.firstName}{" "}
+        <strong>{t("client")}:</strong> {singleAppointment.userId.firstName}{" "}
         {singleAppointment.userId.lastName}
       </p>
       <p>
-        <strong>Therapist:</strong> {singleAppointment.therapistId.firstName}{" "}
+        <strong>{t("therapist")}:</strong>{" "}
+        {singleAppointment.therapistId.firstName}{" "}
         {singleAppointment.therapistId.lastName}
       </p>
       <p>
-        <strong>Date:</strong>{" "}
+        <strong>{t("date")}:</strong>{" "}
         {formatDateTime(singleAppointment.appointmentDate, "date")}
       </p>
       <p>
-        <strong>Time:</strong>{" "}
+        <strong>{t("time")}:</strong>{" "}
         {formatDateTime(singleAppointment.startTime, "time")} -{" "}
         {formatDateTime(singleAppointment.endTime, "time")}
       </p>
@@ -30,7 +33,7 @@ const AppointmentDetails = () => {
           rel="noopener noreferrer"
           className="block mt-2 text-indigo-600 hover:underline"
         >
-          Join Meeting
+          {t("joinMeeting")}
         </a>
       )}
     </div>
