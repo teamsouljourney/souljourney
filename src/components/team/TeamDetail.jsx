b657d6a6d0f7ea53ca9b4de90f1acea882fef881
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-02-20 01:37:58 +0300

    refactor: Update TeamDetail component to match updated Button component styles and layout

--- src/components/team/TeamDetail.jsx
+++ src/components/team/TeamDetail.jsx
@@ -3,7 +3,8 @@ import { useParams, useNavigate } from "react-router-dom";
 import useTherapistCall from "../../hooks/useTherapistCall";
 import { useSelector } from "react-redux";
 import AppointmentCalendar from "../calendar/appointmentCalendar";
-import avatar from "../../assets/avatar.png"
+import avatar from "../../assets/avatar.png";
+import Button from "../button/Button";
 
 const TeamDetail = () => {
   const { id } = useParams();
@@ -13,7 +14,7 @@ const TeamDetail = () => {
     (state) => state.therapists
   );
 
-  const [displayCalendar, setDisplayCalendar] = useState(false)
+  const [displayCalendar, setDisplayCalendar] = useState(false);
 
   useEffect(() => {
     getSingleTherapist(id);
@@ -30,128 +31,171 @@ const TeamDetail = () => {
 
   console.log(singleTherapist);
 
-  const {firstName, lastName, email, image, categoryId, feedbackId, description, graduation, experience} = singleTherapist;
-    
+  const {
+    firstName,
+    lastName,
+    email,
+    image,
+    categoryId,
+    feedbackId,
+    description,
+    graduation,
+    experience,
+  } = singleTherapist;
+
   // console.log(categoryId);
 
-  const therapistCategories = categoryId
+  const therapistCategories = categoryId;
 
   const toggleCalendar = (show) => {
-    setDisplayCalendar(show)
-  }
+    setDisplayCalendar(show);
+  };
 
   const handleOutSideClick = (e) => {
-    if (e.target === e. currentTarget) {
-      toggleCalendar(false)
+    if (e.target === e.currentTarget) {
+      toggleCalendar(false);
     }
-  }
-
-  
+  };
 
   return (
-    <div className="container max-w-none min-h-screen flex flex-col justify-center items-center gap-6 py-3 bg-offWhite dark:bg-background-dark text-navy-dark dark:text-offWhite">
+    <div className="container max-w-none min-h-screen flex flex-col justify-center items-center gap-2 py-3 bg-offWhite dark:bg-background-darker text-navy-dark dark:text-offWhite">
       {/* Header */}
-      <div className="w-full bg-[#FDF6F0] pt-24 pb-16">
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 mx-auto w-full max-w-7xl p-8 mt-10">
-        {/* Profile Pic */}
-        <div className="lg:col-span-3 flex justify-center">
-          <img className="w-48 h-48 rounded-full border-4 border-seaGreen object-cover" src={image || avatar} alt={firstName} />          
-        </div>
+      <div className="w-full bg-offWhite-light dark:bg-background-dark pt-12">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 mx-auto w-full max-w-7xl p-8 mt-6">
+          {/* Profile Pic */}
+          <div className="lg:col-span-3 flex justify-center">
+            <img
+              className="w-48 h-48 rounded-full border-4 border-seaGreen object-cover"
+              src={image || avatar}
+              alt={firstName}
+            />
+          </div>
 
-        {/* Identity */}
-        <div className="lg:col-span-6 flex flex-col text-center lg:text-left">
-          <div className="text-2xl font-semibold">
-            {/* Name */}
-            <div className="text-4xl font-semibold mb-2">
-              {firstName} {lastName}
-              <p className="text-sm mt-1">{email}</p>
-            </div>
-            {/* Categories */}
-            <div className="flex sm:flex-wrap lg:flex-nowrap justify-center lg:justify-start mt-4 gap-2 text-lg">
-              {therapistCategories.map((category) => <span className="px-4 py-1 bg-[#E8F5E9] text-[#2E7D32] rounded-full text-sm" key={category._id}>{category.name}</span> )}
-            </div>
-            {/* Services */}
-            <div className="flex flex-wrap justify-center lg:justify-start items-center mt-2 gap-x-6">
-              
-              <div className="flex flex-row items-center  justify-center lg:justify-start gap-2">
-                <span
-                  style={{
-                    maskImage: `url(/assets/sidebar/videoCall2.svg)`,
-                    maskRepeat: "no-repeat",
-                    maskSize: "contain",
-                    width: "30px",
-                    height: "30px",
-                  }}
-                  className="inline-flex justify-center items-center  bg-navy-light "
-                ></span>
-                
-                <span className="text-[1rem] font-semibold">
-                  Video Call
-                </span>
+          {/* Identity */}
+          <div className="lg:col-span-6 flex flex-col text-center lg:text-left">
+            <div className="text-2xl font-semibold">
+              {/* Name */}
+              <div className="text-4xl font-semibold mb-2">
+                {firstName} {lastName}
+                <p className="text-sm mt-1">{email}</p>
               </div>
-              <div className="flex flex-row items-center  justify-center lg:justify-start gap-2">                
-                <span
-                  style={{
-                    maskImage: `url(/assets/sidebar/chat2.svg)`,
-                    maskRepeat: "no-repeat",
-                    maskSize: "contain",
-                    width: "30px",
-                    height: "30px",
-                  }}
-                  className="inline-flex justify-center items-center  bg-navy-light "
-                ></span>
-                
-                <span className="text-[1rem] font-semibold">
-                  Live Chat
-                </span>
+              {/* Categories */}
+              <div className="flex sm:flex-wrap lg:flex-nowrap justify-center lg:justify-start mt-4 gap-2 text-lg">
+                {therapistCategories.map((category) => (
+                  <span
+                    className="px-4 py-1 bg-[#E8F5E9] text-seaGreen rounded-full text-sm"
+                    key={category._id}
+                  >
+                    {category.name}
+                  </span>
+                ))}
               </div>
-              
+              {/* Services */}
+              <div className="flex flex-wrap justify-center lg:justify-start items-center mt-2 gap-x-6">
+                <div className="flex flex-row items-center  justify-center lg:justify-start gap-2">
+                  <span
+                    style={{
+                      maskImage: `url(/assets/sidebar/videoCall2.svg)`,
+                      maskRepeat: "no-repeat",
+                      maskSize: "contain",
+                      width: "30px",
+                      height: "30px",
+                    }}
+                    className="inline-flex justify-center items-center  bg-navy-light "
+                  ></span>
+
+                  <span className="text-[1rem] font-semibold">Video Call</span>
+                </div>
+                <div className="flex flex-row items-center  justify-center lg:justify-start gap-2">
+                  <span
+                    style={{
+                      maskImage: `url(/assets/sidebar/chat2.svg)`,
+                      maskRepeat: "no-repeat",
+                      maskSize: "contain",
+                      width: "30px",
+                      height: "30px",
+                    }}
+                    className="inline-flex justify-center items-center  bg-navy-light "
+                  ></span>
+
+                  <span className="text-[1rem] font-semibold">Live Chat</span>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* Appointment button */}
-        <div className="lg:col-span-3 flex justify-center lg:justify-end items-center">
-          <button
-            onClick={() => toggleCalendar(true)}
-            className="px-6 py-3 text-lg leading-none text-white rounded-lg bg-gradient-to-r from-navy to-seaGreen hover:bg-gradient-to-r hover:from-navy-dark hover:to-seaGreen-dark transition-transform duration-150 ease-in-out active:scale-95 capitalize"
-          >
-            work with me
-          </button>
+          {/* Appointment button */}
+          <div className="lg:col-span-3 flex justify-center lg:justify-end items-center">
+            <button
+              onClick={() => toggleCalendar(true)}
+              className="px-6 py-3 text-lg leading-none text-white rounded-lg bg-gradient-to-r from-navy to-seaGreen hover:bg-gradient-to-r hover:from-navy-dark hover:to-seaGreen-dark transition-transform duration-150 ease-in-out active:scale-95 capitalize"
+            >
+              work with me
+            </button>
+          </div>
         </div>
       </div>
-      </div>
-      
 
       {/* Body */}
       <div className="w-full px-4">
-        <div className="max-w-7xl mx-auto py-12">
-          
+        <div className="grid grid-cols-1 gap-6 mx-auto w-full max-w-6xl p-8">
+          <div className="row-span-4 mx-auto pb-3 border-b-2">
             <h2 className="text-2xl font-semibold mb-4 ">About</h2>
             <p className=" leading-relaxed">{description}</p>
-          
-        </div>
-        <div>
-          <h2 className="text-2xl font-semibold mb-4 ">Proffesional experience</h2>
-          <div className="p-6 rounded-lg">{experience}</div>
-          <div className="p-6 rounded-lg">{graduation}</div>
-        </div>
-        
-        <div>
-          <h2 className="text-2xl font-semibold mb-4 ">Reviews</h2>
-          <div className="p-6 rounded-lg">{feedbackId.comment}</div>
-        </div>
-        
+          </div>
+          <div className="row-span-3 mx-auto pb-3 border-b-2 w-full">
+            <h2 className="text-2xl font-semibold mb-4 ">
+              Proffesional experience
+            </h2>
+            <div className="">{experience}</div>
+            <div className="">{graduation}</div>
+          </div>          
+          <div className="row-span-2 mx-auto pb-3 border-b-2 w-full">
+            <h2 className="text-2xl font-semibold mb-4 ">Services</h2>
+            <div className="flex flex-wrap justify-center lg:justify-start items-center mt-2 gap-x-6">
+                <div className="flex flex-row items-center  justify-center lg:justify-start gap-2">
+                  <span
+                    style={{
+                      maskImage: `url(/assets/sidebar/videoCall2.svg)`,
+                      maskRepeat: "no-repeat",
+                      maskSize: "contain",
+                      width: "30px",
+                      height: "30px",
+                    }}
+                    className="inline-flex justify-center items-center  bg-navy-light "
+                  ></span>
 
+                  <span className="text-[1rem] font-semibold">Video Call</span>
+                </div>
+                <div className="flex flex-row items-center  justify-center lg:justify-start gap-2">
+                  <span
+                    style={{
+                      maskImage: `url(/assets/sidebar/chat2.svg)`,
+                      maskRepeat: "no-repeat",
+                      maskSize: "contain",
+                      width: "30px",
+                      height: "30px",
+                    }}
+                    className="inline-flex justify-center items-center  bg-navy-light "
+                  ></span>
+
+                  <span className="text-[1rem] font-semibold">Live Chat</span>
+                </div>
+              </div>
+          </div>
+          <div className="row-span-2 mx-auto pb-3 border-b-2 w-full">
+            <h2 className="text-2xl font-semibold mb-4 ">Reviews</h2>
+            <div className="">{feedbackId.comment}</div>
+            <p className=" leading-relaxed">We truly appreciate the wonderful feedback we receive about our professionals. Each review reflects the genuine experiences of those who have worked with them, shared voluntarily by individuals who found value in their journey. Everyone’s therapy experience is unique, and we are grateful for the trust placed in us.</p>
+            
+          </div>
+        </div>
       </div>
       {/* GoBack */}
       <div className="p-6 pt-0 mt-8 text-center w-full">
-        <button
-          onClick={() => navigate(-1)}
-          className="px-6 py-3 text-lg text-white rounded-lg bg-gradient-to-r from-navy to-seaGreen hover:bg-gradient-to-r hover:from-navy-dark hover:to-seaGreen-dark transition-transform duration-150 ease-in-out active:scale-95"
-        >
+        <Button onClick={() => navigate(-1)} type="type22">
           Go Back
-        </button>
+        </Button>
       </div>
 
       {/* Appointment */}
@@ -165,8 +209,6 @@ const TeamDetail = () => {
           </div>
         </div>
       )}
-      
-      
     </div>
   );
 };
