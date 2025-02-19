1ff6ab7060af571a8f30fc7ae980a148530fc9d0
ucangun <149247682+ucangun@users.noreply.github.com>
2025-02-19 15:11:23 +0100

    feat: updated SidebarListItems and AppRouter to include new routes and menu items

--- src/components/sidebar/SidebarListItems.jsx
+++ src/components/sidebar/SidebarListItems.jsx
@@ -8,9 +8,31 @@ const NavLinkto = (to) => `/profile/${to}`;
 const SidebarListItems = () => {
   const { t } = useTranslation();
   const { currentUser } = useSelector((state) => state.auth);
-  let menu = [];
-  currentUser?.isTherapist && (
-    menu = [
+  console.log(currentUser.isAdmin);
+  let menu = [
+    {
+      to: NavLinkto(""),
+      icon: icon("dashboard"),
+      name: t("dashboard"),
+    },
+    {
+      to: NavLinkto("appointment"),
+      icon: icon("appointment"),
+      name: t("appointment"),
+    },
+    {
+      to: NavLinkto("chat"),
+      icon: icon("chat"),
+      name: t("chat"),
+    },
+    {
+      to: NavLinkto("video-call"),
+      icon: icon("videoCall"),
+      name: t("videoCall"),
+    },
+  ];
+  currentUser?.isTherapist &&
+    (menu = [
       {
         to: NavLinkto(""),
         icon: icon("dashboard"),
@@ -36,12 +58,11 @@ const SidebarListItems = () => {
         icon: icon("videoCall"),
         name: t("videoCall"),
       },
-    ]
-  );
-  currentUser?.isAdmin && (
-    menu = [
+    ]);
+  currentUser?.isAdmin &&
+    (menu = [
       {
-        to: NavLinkto(""),
+        to: NavLinkto("user-management"),
         icon: icon("listUser1"),
         name: t("userManagement"),
       },
@@ -56,48 +77,27 @@ const SidebarListItems = () => {
         name: t("categoryManagement"),
       },
       {
+        to: NavLinkto("appointment-management"),
+        icon: icon("appointment2"),
+        name: t("appointmentManagement"),
+      },
+      {
         to: NavLinkto("blog-management"),
         icon: icon("blog1"),
         name: t("blogManagement"),
-      },      
+      },
       {
         to: NavLinkto("feedback-management"),
         icon: icon("feedback2"),
         name: t("feedbackManagement"),
       },
-      {
-        to: NavLinkto("appointment-management"),
-        icon: icon("appointment2"),
-        name: t("appontmentManagement"),
-      },
-    ]
-  );
-  menu = [{
-    to: NavLinkto(""),
-    icon: icon("dashboard"),
-    name: t("dashboard"),
-  },
-  {
-    to: NavLinkto("appointment"),
-    icon: icon("appointment"),
-    name: t("appointment"),
-  },
-  {
-    to: NavLinkto("chat"),
-    icon: icon("chat"),
-    name: t("chat"),
-  },
-  {
-    to: NavLinkto("video-call"),
-    icon: icon("videoCall"),
-    name: t("videoCall"),
-  },]
+    ]);
 
   return (
     <>
-      <div className="flex flex-col h-full overflow-y-auto overflow-x-hidden flex-grow pt-2 justify-between">
-        <div className="flex flex-col  space-y-1 mx-1 lg:mt-1 ">
-          {/* <div className="px-5 pt-4 hidden lg:block">
+      <div className="flex flex-col justify-between flex-grow h-full pt-2 overflow-x-hidden overflow-y-auto">
+        <div className="flex flex-col mx-1 space-y-1 lg:mt-1 ">
+          {/* <div className="hidden px-5 pt-4 lg:block">
             <div className="flex flex-row items-center">
               <div className="text-sm font-bold tracking-wide text-seaGreen-dark">
                 {t("menu")}
@@ -121,7 +121,7 @@ const SidebarListItems = () => {
             >
               {({ isActive }) => (
                 <>
-                  <span className="inline-flex justify-center items-center ml-1">
+                  <span className="inline-flex items-center justify-center ml-1">
                     <span
                       style={{
                         maskImage: `url(${item.icon})`,
@@ -148,53 +148,52 @@ const SidebarListItems = () => {
           ))}
         </div>
         {!currentUser?.isAdmin && (
-          <div className="flex flex-col  space-y-1 mx-1 mb-2 lg:mt-1 ">
-          <NavLink
-            className={({ isActive }) =>
-              `group flex flex-row items-center  justify-center lg:justify-start rounded-md h-12 focus:outline-none pr-1.5 lg:pr-6 font-semibold text-navy-dark hover:text-seaGreen-dark cursor-pointer ${
-                isActive
-                  ? "bg-seaGreen-dark/20 text-seaGreen-dark dark:bg-offWhite-dark/20 dark:text-offWhite-dark"
-                  : ""
-              }`
-            }
-            to="/profile/account"
-            draggable="false"
-          >
-            {({ isActive }) => (
-              <>
-                <span className="inline-flex justify-center items-center ml-1">
+          <div className="flex flex-col mx-1 mb-2 space-y-1 lg:mt-1 ">
+            <NavLink
+              className={({ isActive }) =>
+                `group flex flex-row items-center  justify-center lg:justify-start rounded-md h-12 focus:outline-none pr-1.5 lg:pr-6 font-semibold text-navy-dark hover:text-seaGreen-dark cursor-pointer ${
+                  isActive
+                    ? "bg-seaGreen-dark/20 text-seaGreen-dark dark:bg-offWhite-dark/20 dark:text-offWhite-dark"
+                    : ""
+                }`
+              }
+              to="/profile/account"
+              draggable="false"
+            >
+              {({ isActive }) => (
+                <>
+                  <span className="inline-flex items-center justify-center ml-1">
+                    <span
+                      style={{
+                        maskImage: `url(/assets/sidebar/settings.svg)`,
+                        maskRepeat: "no-repeat",
+                        maskSize: "contain",
+                        width: "20px",
+                        height: "20px",
+                      }}
+                      className={`inline-flex justify-center items-center mx-3.5 bg-navy-dark dark:bg-offWhite-dark group-hover:bg-seaGreen-dark dark:group-hover:bg-seaGreen-light ${
+                        isActive ? "bg-seaGreen-dark" : ""
+                      }`}
+                    ></span>
+                  </span>
                   <span
-                    style={{
-                      maskImage: `url(/assets/sidebar/settings.svg)`,
-                      maskRepeat: "no-repeat",
-                      maskSize: "contain",
-                      width: "20px",
-                      height: "20px",
-                    }}
-                    className={`inline-flex justify-center items-center mx-3.5 bg-navy-dark dark:bg-offWhite-dark group-hover:bg-seaGreen-dark dark:group-hover:bg-seaGreen-light ${
-                      isActive ? "bg-seaGreen-dark" : ""
+                    className={`ml-0 lg:ml-2 text-sm text-navy-dark group-hover:text-seaGreen-dark dark:group-hover:text-seaGreen-light dark:text-offWhite-dark tracking-wide truncate capitalize hidden lg:block ${
+                      isActive ? "text-seaGreen-dark" : ""
                     }`}
-                  ></span>
-                </span>
-                <span
-                  className={`ml-0 lg:ml-2 text-sm text-navy-dark group-hover:text-seaGreen-dark dark:group-hover:text-seaGreen-light dark:text-offWhite-dark tracking-wide truncate capitalize hidden lg:block ${
-                    isActive ? "text-seaGreen-dark" : ""
-                  }`}
-                >
-                  {t("myAccount")}
-                </span>
-              </>
-            )}
-          </NavLink>
-        </div>
+                  >
+                    {t("myAccount")}
+                  </span>
+                </>
+              )}
+            </NavLink>
+          </div>
         )}
-        
       </div>
       <hr className="xs:hidden lg:block " />
       {/* Sidebar footer */}
       {/* <div className="px-1" onClick={()=>logout()}>
         <div className="group flex flex-row items-center  justify-center lg:justify-start rounded-md h-12 focus:outline-none pr-1.5 lg:pr-6 font-semibold text-navy-dark dark:text-offWhite-dark hover:text-pink-light cursor-pointer  ">
-          <span className="inline-flex justify-center items-center ml-1">
+          <span className="inline-flex items-center justify-center ml-1">
             <span
               style={{
                 maskImage: `url(/assets/sidebar/sign-out-2.svg)`,
@@ -206,7 +205,7 @@ const SidebarListItems = () => {
               className="inline-flex justify-center items-center mx-3.5 bg-red-400 group-hover:bg-red-400 dark:bg-pink-light"
             ></span>
           </span>
-          <span className="ml-2 text-sm text-navy-dark group-hover:text-red-400 dark:text-offWhite-dark tracking-wide truncate capitalize hidden lg:block">
+          <span className="hidden ml-2 text-sm tracking-wide capitalize truncate text-navy-dark group-hover:text-red-400 dark:text-offWhite-dark lg:block">
             {t("logout")}
           </span>
         </div>
