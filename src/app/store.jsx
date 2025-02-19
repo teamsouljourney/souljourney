f84053c3293fcaa47a6523b735497a27d5ea37fc
ayseugurlu <ayseugurlu9135@gmail.com>
2025-02-19 09:44:21 +0100

    wip1

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -6,6 +6,7 @@ import therapistReducer from "../features/therapistSlice";
 import noteReducer from "../features/noteSlice";
 import blogReducer from "../features/blogSlice";
 import calendarReducer from "../features/calendarSlice";
+import chatReducer from "../features/chatSlice";
 
 //*redux-persist
 import {
@@ -38,6 +39,7 @@ const store = configureStore({
     notes: noteReducer,
     blogs: blogReducer,
     calendar: calendarReducer,
+    chats: chatReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
