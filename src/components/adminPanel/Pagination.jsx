ab597fed16e138fb46f0f267e32fe789fd7c7ed1
ucangun <149247682+ucangun@users.noreply.github.com>
2025-02-23 02:46:26 +0100

    feat: Added URL parameter management for pagination and refactored code to use it

--- src/components/adminPanel/Pagination.jsx
+++ src/components/adminPanel/Pagination.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useSearchParams, useNavigate } from "react-router-dom"; // URL yönetimi için
 import usePaginationCall from "../../hooks/usePaginationCall";
 import { setPage } from "../../features/paginationSlice";
 
 const Pagination = ({ endpoint, slice, data }) => {
   const dispatch = useDispatch();
+  const [searchParams, setSearchParams] = useSearchParams();
   const { getDataByPage } = usePaginationCall();
   const { currentPage, itemsPerPage } = useSelector(
     (state) => state.pagination
@@ -12,17 +14,20 @@ const Pagination = ({ endpoint, slice, data }) => {
 
   const totalPages = Math.ceil(data.length / itemsPerPage);
 
-  const pageButtons = Array.from(
-    { length: totalPages },
-    (_, index) => index + 1
-  );
+  const pageFromUrl = Number(searchParams.get("page")) || 1;
 
   useEffect(() => {
-    getDataByPage(endpoint, slice, itemsPerPage, currentPage);
-  }, [itemsPerPage, currentPage]);
+    if (!searchParams.get("page")) {
+      setSearchParams({ page: 1 }, { replace: true });
+    } else {
+      dispatch(setPage(pageFromUrl));
+      getDataByPage(endpoint, slice, itemsPerPage, pageFromUrl);
+    }
+  }, [searchParams]);
 
   const handlePageChange = (page) => {
     if (page > 0 && page <= totalPages) {
+      setSearchParams({ page });
       dispatch(setPage(page));
     }
   };
@@ -41,19 +46,21 @@ const Pagination = ({ endpoint, slice, data }) => {
           Previous
         </button>
 
-        {pageButtons.map((page) => (
-          <button
-            key={page}
-            onClick={() => handlePageChange(page)}
-            className={`px-3 py-1 text-sm rounded ${
-              currentPage === page
-                ? "bg-mauve hover:bg-mauve-dark text-white"
-                : "hover:bg-mauve opacity-50"
-            }`}
-          >
-            {page}
-          </button>
-        ))}
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+          (page) => (
+            <button
+              key={page}
+              onClick={() => handlePageChange(page)}
+              className={`px-3 py-1 text-sm rounded ${
+                currentPage === page
+                  ? "bg-mauve hover:bg-mauve-dark text-white"
+                  : "hover:bg-mauve opacity-50"
+              }`}
+            >
+              {page}
+            </button>
+          )
+        )}
 
         <button
           onClick={() => handlePageChange(currentPage + 1)}
