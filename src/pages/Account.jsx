347c7b562cb424ea8c88e4a6c5178d4e4b84392d
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-02-23 08:11:37 +0300

    refactor: Update password reset form to use AccountChangePasswordForm component

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -11,6 +11,7 @@ import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import AccountForm from "../components/auth/AccountForm";
+import AccountChangePasswordForm from "../components/auth/AccountChangePasswordForm";
 
 const Account = () => {
   const { t } = useTranslation();
@@ -77,114 +78,11 @@ const Account = () => {
           </div>
         </div>
       </div>
-      {/* ------- */}
-      {/* <div className="flex h-px w-full flex-none flex-col items-center bg-neutral-border" /> */}
+      
 
-      {/* Reset Password Field */}
+      {/* Change Password Field */}
 
-      <div className="flex w-full max-w-[576px] flex-col items-start gap-4 border-b-2 border-b-gray-200 dark:border-b-gray-400">
-        <span className="text-lg font-medium">{t("password")}</span>
-        {/* Password input */}
-
-        <div className="sm:col-span-4 relative">
-          <label htmlFor="password" className="password-label">
-            Current Password
-          </label>
-          <div className="mt-2 relative">
-            <input
-              id="password"
-              name="password"
-              type={showPassword ? "text" : "password"}
-              placeholder="Enter current password"
-              autoComplete="current-password"
-              className="peer min-w-[280px]"
-            />
-            <div
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute inset-y-0 right-2 flex items-center px-2 text-gray-500 hover:text-gray-700 cursor-pointer"
-            >
-              <img
-                src={
-                  showPassword ? "/assets/visible.svg" : "/assets/invisible.svg"
-                }
-                alt={showPassword ? "Hide password" : "Show password"}
-                className="w-4 h-4  opacity-60 hover:opacity-80 transition-opacity"
-                draggable="false"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="sm:col-span-4 relative">
-          <label htmlFor="new-password" className="password-label">
-            New Password
-          </label>
-          <div className="mt-2 relative">
-            <input
-              id="new-password"
-              name="new-password"
-              type={showPassword ? "text" : "password"}
-              placeholder="Enter new password"
-              autoComplete="current-password"
-              className="peer min-w-[280px]"
-            />
-            <div
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute inset-y-0 right-2 flex items-center px-2 text-gray-500 hover:text-gray-700 cursor-pointer"
-            >
-              <img
-                src={
-                  showPassword ? "/assets/visible.svg" : "/assets/invisible.svg"
-                }
-                alt={showPassword ? "Hide password" : "Show password"}
-                className="w-4 h-4  opacity-60 hover:opacity-80 transition-opacity"
-                draggable="false"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="sm:col-span-4 relative">
-          <label htmlFor="retype-password" className="password-label">
-            Re-type New Password
-          </label>
-          <div className="mt-2 relative">
-            <input
-              id="retype-password"
-              name="retype-password"
-              type={showPassword ? "text" : "password"}
-              placeholder="Re-type new password"
-              autoComplete="retype-password"
-              className="peer min-w-[280px]"
-            />
-            <div
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute inset-y-0 right-2 flex items-center px-2 text-gray-500 hover:text-gray-700 cursor-pointer"
-            >
-              <img
-                src={
-                  showPassword ? "/assets/visible.svg" : "/assets/invisible.svg"
-                }
-                alt={showPassword ? "Hide password" : "Show password"}
-                className="w-4 h-4  opacity-60 hover:opacity-80 transition-opacity"
-                draggable="false"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="flex w-full flex-col items-start justify-center gap-6 mt-6">
-          <button type="button" className="account-btn mb-4 w-1/2">
-            Change Password
-          </button>
-        </div>
-      </div>
-
-      {/* ------- */}
-      {/* <div className="flex h-px w-full flex-none flex-col items-center bg-neutral-border " /> */}
+      <AccountChangePasswordForm/>
 
       {/* Delete Account Field */}
 
