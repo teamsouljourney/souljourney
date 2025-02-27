4f6b9ebb8510fa325c47ece3cd7e6cc837e8adb4
ucangun <149247682+ucangun@users.noreply.github.com>
2025-02-27 09:30:33 +0100

    feat: updated useAxios hook and i18n configuration

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,9 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import enTranslation from "./locales/en.json";
 import deTranslation from "./locales/de.json";
 import trTranslation from "./locales/tr.json";
-import LanguageDetector from "i18next-browser-languagedetector";
-import HttpApi from "i18next-http-backend";
 
 const resources = {
   en: { translation: enTranslation },
@@ -12,37 +11,25 @@ const resources = {
   de: { translation: deTranslation },
 };
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: localStorage.getItem("language") || "en",
-  fallbackLng: "en",
-  // debug: true,
-  interpolation: {
-    escapeValue: false,
-  },
-});
-
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    supportedLngs: ["en", "tr", "de"],
+    lng: localStorage.getItem("language") || "en",
+    fallbackLng: "en",
+    detection: {
+      order: ["localStorage", "cookie", "navigator"],
+      caches: ["localStorage", "cookie"],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 i18n.on("languageChanged", (lng) => {
   localStorage.setItem("language", lng);
 });
 
-i18n
-  .use(HttpApi)  
-  .use(LanguageDetector)  
-  .use(initReactI18next)  
-  .init({
-    supportedLngs: ["en", "tr", "de"],  
-    fallbackLng: "en",  
-    detection: {
-      order: ["localStorage", "cookie", "navigator"],  
-      caches: ["localStorage", "cookie"],  
-    },
-    backend: {
-      loadPath: "/locales/{{lng}}/translation.json",  
-    },
-    interpolation: {
-      escapeValue: false,  
-    },
-  });
 export default i18n;
