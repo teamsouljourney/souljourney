59ff705e5c094e797b716ae473771062362081d6
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-03-01 19:22:39 +0300

    feat: Added optional chaining in MyBlogList.jsx for null checks

--- src/components/dashboard/MyBlogList.jsx
+++ src/components/dashboard/MyBlogList.jsx
@@ -26,11 +26,11 @@ export default function MyBlogList() {
     getAllBlogs();
   }, []);
 
-  const filteredBlogsByTherapist = blogs.filter(
-    (blog) => blog.therapistId._id === currentUser._id
+  const filteredBlogsByTherapist = blogs?.filter(
+    (blog) => blog?.therapistId?._id === currentUser?._id
   );
 
-  if (!currentUser.isTherapist) return null;
+  if (!currentUser?.isTherapist) return null;
 
   return (
     <Container
@@ -79,7 +79,7 @@ export default function MyBlogList() {
       >
         {filteredBlogsByTherapist?.map((item) => (
           <ImageListItem
-            key={item.title}
+            key={item?.title}
             sx={{
               width: "100%",
               height: "100%",
@@ -89,9 +89,9 @@ export default function MyBlogList() {
                 cursor: "pointer",
               },
             }}
-            onClick={() => navigate(`/blogs/${item._id}`)}
+            onClick={() => navigate(`/blogs/${item?._id}`)}
           >
-            <img src={item.image} alt={item.title} loading="lazy" />
+            <img src={item?.image} alt={item?.title} loading="lazy" />
             <ImageListItemBar
               title={
                 <Typography
@@ -105,10 +105,10 @@ export default function MyBlogList() {
                     textOverflow: "ellipsis",
                   }}
                 >
-                  {item.title}
+                  {item?.title}
                 </Typography>
               }
-              subtitle={new Date(item.createdAt).toLocaleDateString("de-DE")}
+              subtitle={new Date(item?.createdAt).toLocaleDateString("de-DE")}
               actionIcon={
                 <IconButton
                   sx={{
@@ -120,10 +120,10 @@ export default function MyBlogList() {
                     position: "relative",
                     zIndex: 10,
                   }}
-                  aria-label={`info about ${item.therapistId.title}`}
+                  aria-label={`info about ${item?.therapistId?.title}`}
                   onClick={(event) => {
                     event.stopPropagation();
-                    navigate(`/profile/write-blog/${item._id}`);
+                    navigate(`/profile/write-blog/${item?._id}`);
                   }}
                 >
                   <EditNoteRoundedIcon />
