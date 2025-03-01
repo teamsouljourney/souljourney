2ce6cc9b3b3bd2a7afd865e3c3c781f3f7e2fb22
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-03-01 18:38:10 +0300

    refactor: Update ServicesDetail component and Services page layout

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,14 +4,14 @@ import ServicesDetail from "../components/ServicesPage/ServicesDetail"
 
 const Services = () => {
   return (
-    <div className="flex flex-col bg-offWhite dark:bg-background-dark text-navy dark:text-offWhite-dark">
+    <div className="flex flex-col justify-center items-center bg-offWhite dark:bg-background-dark text-navy dark:text-offWhite-dark">
       {/* Services Hero Section: %20 */}
       <div className="w-full h-1/5">
         <ServicesHeroSection />
       </div>
 
       {/* Services Details: %60 */}
-      <div className="w-full h-3/5">
+      <div className="w-full max-w-7xl flex justify-center h-3/5">
         <ServicesDetail />
       </div>
 
