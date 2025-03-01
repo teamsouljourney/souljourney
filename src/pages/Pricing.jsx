c96fa55141854c10d4d62a0121ab1a9eb952ce50
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-03-01 19:11:41 +0300

    refactor: Update pricing page components: update styles and remove unused classes

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -5,9 +5,12 @@ import PricingForm from "../components/pricing/PricingForm";
 
 const Pricing = () => {
     return (
-      <div className="min-h-screen bg-offWhite-light p-16">
+      <div className="min-h-screen bg-offWhite dark:bg-background-dark text-navy dark:text-offWhite-dark px-16 py-32">
+      <div className="dark:bg-background-darker">
         <PricingHeader />
         <PricingForm />
+      </div>
+        
       </div>
     )
   }
