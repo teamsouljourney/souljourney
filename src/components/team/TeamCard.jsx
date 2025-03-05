ef771f3092b51ed038c7a51d0cad136ed65de0d5
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-03-05 19:07:35 +0300

    refactor: Fixed typos and whitespace in TeamCard component and removed console.log statement in TabSwitch component

--- src/components/team/TeamCard.jsx
+++ src/components/team/TeamCard.jsx
@@ -2,15 +2,14 @@
 import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
-const TeamCard = ({therapist}) => {
+const TeamCard = ({ therapist }) => {
   const { t } = useTranslation();
 
-// console.log(therapist);
+  // console.log(therapist);
 
   return (
-    <div
-      className="flex flex-col w-full max-w-xs  overflow-hidden transition duration-500 transform bg-white shadow-lg dark:bg-background-dark text-navy dark:text-offWhite-dark rounded-xl md:hover:scale-[1.03]"
-    >
+    <div className="flex flex-col w-full max-w-xs  overflow-hidden transition duration-500 transform bg-white shadow-lg dark:bg-background-dark text-navy dark:text-offWhite-dark rounded-xl md:hover:scale-[1.03]">
+      {/* Card Header Image */}
       <div className="relative w-full h-56 md:h-64">
         <img
           className="object-cover w-full h-full "
@@ -18,20 +17,21 @@ const TeamCard = ({therapist}) => {
           alt={`${therapist?.firstName} ${therapist?.lastName}`}
         />
       </div>
-
+      {/* Card Body */}
       <div className="flex flex-col justify-around gap-y-2 flex-grow p-6 cursor-default">
         <h2 className="mb-2 text-xl lg:text-2xl font-bold">
           {`${therapist?.firstName} ${therapist?.lastName}`}
         </h2>
         <div className="mb-2 text-sm md:text-base font-bold text-seaGreen-dark dark:text-seaGreen-light">
-          {therapist?.categoryId?.map((category)=> (
-          <i key={category._id}>{category.name}</i>
-        ))}
+          {therapist?.categoryId?.map((category) => (
+            <i key={category._id}>{category.name}</i>
+          ))}
         </div>
-        
-        <p className="line-clamp-2 text-xs sm:text-sm md:text-base">{therapist?.description}</p>
+        <p className="line-clamp-2 text-xs sm:text-sm md:text-base">
+          {therapist?.description}
+        </p>
       </div>
-
+      {/* Card Footer See More Button */}
       <div className="flex items-center justify-center px-4 py-3 md:py-4 mt-auto border-t border-gray-300">
         <NavLink
           to={`/therapists/${therapist?._id}`}
