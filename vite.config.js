2d9de7650d507729ed3a38539a8033898444c80a
ucangun <149247682+ucangun@users.noreply.github.com>
2025-03-08 23:19:46 +0100

    feat: Configure Vite server for HTTPS in development and update .gitignore for certificate files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const isDevelopment = mode === "development";
+
+  return {
+    plugins: [react()],
+    server: isDevelopment
+      ? {
+          https: {
+            key: fs.readFileSync("./certs/localhost+2-key.pem"),
+            cert: fs.readFileSync("./certs/localhost+2.pem"),
+          },
+        }
+      : {},
+  };
+});
