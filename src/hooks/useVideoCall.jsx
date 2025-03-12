633a309e99bd1757446c8414a0487e171d20b4ed
ucangun <149247682+ucangun@users.noreply.github.com>
2025-03-12 22:05:30 +0100

    refactor: remove unused TURN server configurations from useVideoCall hook

--- src/hooks/useVideoCall.jsx
+++ src/hooks/useVideoCall.jsx
@@ -23,17 +23,6 @@ const iceServers = {
     { urls: "stun:stun2.l.google.com:19302" },
     { urls: "stun:stun3.l.google.com:19302" },
     { urls: "stun:stun4.l.google.com:19302" },
-
-    {
-      urls: "turn:numb.viagenie.ca",
-      username: "webrtc@live.com",
-      credential: "muazkh",
-    },
-    {
-      urls: "turn:turn.anyfirewall.com:443?transport=tcp",
-      username: "webrtc",
-      credential: "webrtc",
-    },
   ],
   iceCandidatePoolSize: 10,
 };
