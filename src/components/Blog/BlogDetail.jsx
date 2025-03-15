1595f4cbe0efcc0ed1ca13143d63329bd44407f4
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-03-15 15:43:54 +0300

    feat: Added HiEye icon and countOfVisitors to BlogDetail component

--- src/components/Blog/BlogDetail.jsx
+++ src/components/Blog/BlogDetail.jsx
@@ -1,11 +1,10 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import { HiOutlineHeart, HiHeart, HiArrowLeft } from "react-icons/hi";
+import { HiOutlineHeart, HiHeart, HiArrowLeft, HiEye } from "react-icons/hi";
 import useBlogCall from "../../hooks/useBlogCall";
 import { useDispatch, useSelector } from "react-redux";
 import { getSingleBlogSuccess } from "../../features/blogSlice";
-import { useTranslation } from 'react-i18next';
-
+import { useTranslation } from "react-i18next";
 
 const BlogDetail = () => {
   const { t } = useTranslation();
@@ -37,6 +36,8 @@ const BlogDetail = () => {
       </div>
     );
   }
+  console.log(singleBlog);
+  
 
   return (
     <div className="min-h-screen py-32 mx-auto bg-offWhite dark:bg-background-darker text-navy dark:text-offWhite-dark">
@@ -50,57 +51,72 @@ const BlogDetail = () => {
         </div>
 
         <div className="relative px-10 pt-5 pb-16 mx-5 -mt-16 bg-white rounded-lg shadow-lg dark:bg-background-dark text-navy dark:text-offWhite-dark md:mx-10">
-          <div className="flex items-center gap-2 mb-4">
-            {singleBlog?.therapistId && (
-              <>
-                <img
-                  src={singleBlog?.therapistId?.image || "/placeholder.svg"}
-                  alt={`${singleBlog?.therapistId?.firstName} ${singleBlog?.therapistId?.lastName}`}
-                  className="object-cover w-10 h-10 rounded-full"
-                />
-                <div>
-                  <h3 className="font-semibold md:text-xl">
-                    {singleBlog?.therapistId?.firstName}{" "}
-                    {singleBlog?.therapistId?.lastName}
-                  </h3>
-                  <p className="text-sm">
-                    {singleBlog?.therapistId?.description}
-                  </p>
-                </div>
-              </>
+          <div>
+            <div className="flex items-center gap-2 mb-4">
+              {singleBlog?.therapistId && (
+                <>
+                  <img
+                    src={singleBlog?.therapistId?.image || "/placeholder.svg"}
+                    alt={`${singleBlog?.therapistId?.firstName} ${singleBlog?.therapistId?.lastName}`}
+                    className="object-cover w-10 h-10 rounded-full"
+                  />
+                  <div>
+                    <h3 className="font-semibold md:text-xl">
+                      {singleBlog?.therapistId?.firstName}{" "}
+                      {singleBlog?.therapistId?.lastName}
+                    </h3>
+                    <p className="text-sm">
+                      {singleBlog?.therapistId?.description}
+                    </p>
+                  </div>
+                </>
+              )}
+            </div>
+
+            <h2 className="mb-4 text-xl md:text-2xl font-bold">
+              {singleBlog?.title}
+            </h2>
+            <div
+              className="leading-relaxed text-base md:text-xl"
+              dangerouslySetInnerHTML={{ __html: singleBlog?.content || "" }}
+            ></div>
+
+            {singleBlog?.categoryId && (
+              <span className="inline-block px-4 py-1 mt-6 text-sm rounded-full">
+                {singleBlog?.categoryId?.name}
+              </span>
             )}
           </div>
 
-          <h2 className="mb-4 text-xl md:text-2xl font-bold">{singleBlog?.title}</h2>
-          <div
-            className="leading-relaxed text-base md:text-xl"
-            dangerouslySetInnerHTML={{ __html: singleBlog?.content || "" }}
-          ></div>
-
-          {singleBlog?.categoryId && (
-            <span className="inline-block px-4 py-1 mt-6 text-sm rounded-full">
-              {singleBlog?.categoryId?.name}
-            </span>
-          )}
+          <div className="flex justify-end items-center gap-2">
+            <div className="flex justify-between gap-x-8">
+              <div className="flex items-center gap-x-2">
+                <button
+                  onClick={handleLike}
+                  className="flex items-center transition-colors duration-200 focus:outline-none"
+                >
+                  {isLikedByUser ? (
+                    <HiHeart className="w-7 h-7 text-red-500" />
+                  ) : (
+                    <HiOutlineHeart className="w-6 h-6 hover:text-red-500" />
+                  )}
+                </button>
+                <span className="font-medium text-gray-600 dark:text-gray-300">
+                  {singleBlog?.likes?.length || 0}
+                </span>
+              </div>
+              <div className="flex items-center gap-x-2">
+                <HiEye className="w-7 h-7 dark:text-seaGreen-dark" />
+                <span className="font-medium text-gray-600 dark:text-gray-300">
+                  {singleBlog?.countOfVisitors || 0}
+                </span>
+              </div>
+              
+            </div>
+          </div>
         </div>
 
-        <div className="flex items-center justify-between px-5 mt-6">
-          <div className="flex items-center gap-2">
-            <button
-              onClick={handleLike}
-              className="flex items-center transition-colors duration-200 focus:outline-none"
-            >
-              {isLikedByUser ? (
-                <HiHeart className="w-6 h-6 text-red-500" />
-              ) : (
-                <HiOutlineHeart className="w-6 h-6 hover:text-red-500" />
-              )}
-            </button>
-            <span className="font-medium text-gray-600">
-              {singleBlog?.likes?.length || 0}
-            </span>
-          </div>
-
+        <div className="flex items-center justify-end px-5 mt-6">
           <button
             onClick={() => navigate("/blogs")}
             className="flex items-center gap-2 px-6 py-2 text-white transition-all rounded bg-navy hover:bg-navy-light"
