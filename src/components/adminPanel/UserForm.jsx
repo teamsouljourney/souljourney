166d2f3dc6a8873a08e3f5e2e34602f88d86ae12
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-03-16 08:20:12 +0300

    feat: Add i18n support to admin panel components and pages

--- src/components/adminPanel/UserForm.jsx
+++ src/components/adminPanel/UserForm.jsx
@@ -6,8 +6,10 @@ import {
   setNewUser,
   toggleModal,
 } from "../../features/userSlice";
+import { useTranslation } from "react-i18next";
 
 const UserForm = () => {
+  const { t } = useTranslation();
   const dispatch = useDispatch();
   const { newUser } = useSelector((state) => state.users);
   const { createUser, updateUser } = useUserCall();
@@ -39,13 +41,13 @@ const UserForm = () => {
     <div className="inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
       <div className="px-4 pt-5 pb-4 bg-white dark:bg-background-dark sm:p-6 sm:pb-4">
         <h3 className="text-lg font-medium leading-6 text-navy dark:text-offWhite-dark">
-          {newUser._id ? "Edit User" : "Add a new User"}
+          {newUser._id ? t("AP-editUser") : t("AP-addUser")}
         </h3>
         <div className="mt-2">
           <input
             type="text"
             name="userName"
-            placeholder="Username"
+            placeholder={t("username")}
             value={newUser.userName || ""}
             onChange={handleInputChange}
             className="w-full px-3 py-2 mt-2 border rounded-md border-navy focus:outline-none focus:ring-mauve-dark focus:border-mauve-dark"
@@ -53,7 +55,7 @@ const UserForm = () => {
           <input
             type="email"
             name="email"
-            placeholder="Email"
+            placeholder={t("email")}
             value={newUser.email || ""}
             onChange={handleInputChange}
             className="w-full px-3 py-2 mt-2 border rounded-md border-navy focus:outline-none focus:ring-mauve-dark focus:border-mauve-dark"
@@ -61,7 +63,7 @@ const UserForm = () => {
           <input
             type="text"
             name="firstName"
-            placeholder="First Name"
+            placeholder={t("firstName")}
             value={newUser.firstName || ""}
             onChange={handleInputChange}
             className="w-full px-3 py-2 mt-2 border rounded-md border-navy focus:outline-none focus:ring-mauve-dark focus:border-mauve-dark"
@@ -69,7 +71,7 @@ const UserForm = () => {
           <input
             type="text"
             name="lastName"
-            placeholder="Last Name"
+            placeholder={t("lastName")}
             value={newUser.lastName || ""}
             onChange={handleInputChange}
             className="w-full px-3 py-2 mt-2 border rounded-md border-navy focus:outline-none focus:ring-mauve-dark focus:border-mauve-dark"
@@ -78,7 +80,7 @@ const UserForm = () => {
             id="password"
             name="password"
             type={showPassword ? "text" : "password"}
-            placeholder="Enter password"
+            placeholder={t("passwordPlaceholder")}
             value={newUser.password || ""}
             onChange={handleInputChange}
             disabled={!!newUser._id}
@@ -93,14 +95,14 @@ const UserForm = () => {
           className="inline-flex justify-center w-full px-4 py-2 text-white duration-300 rounded-md bg-seaGreen transtiton hover:bg-navy sm:ml-3 sm:w-auto"
           onClick={handleSubmit}
         >
-          {newUser._id ? "Update User" : "Add User"}
+          {newUser._id ? t("updateUser") : t("addUser")}
         </button>
         <button
           type="button"
           className="inline-flex justify-center w-full px-4 py-2 mt-3 text-gray-700 bg-white rounded-md hover:bg-gray-50 sm:mt-0 sm:ml-3 sm:w-auto"
           onClick={handleCloseModal}
         >
-          Close
+          {t("close")} {/* close */}
         </button>
       </div>
     </div>
