350226c203de097bad1a73b0044d6cfecd311dfe
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-03-16 18:47:10 +0300

    feat: Added new translations and updated button text in TherapistManagement component

--- src/pages/TherapistManagement.jsx
+++ src/pages/TherapistManagement.jsx
@@ -54,7 +54,7 @@ const TherapistManagement = () => {
                 className="px-4 py-2 text-white transition duration-300 rounded-md bg-seaGreen hover:bg-navy"
               >
                 <PlusIcon className="inline-block w-5 h-5 mr-2" />
-                Add a new Therapist
+                {t("addNewTherapist")} {/* Add a new Therapist */}
               </button>
             }
           />
