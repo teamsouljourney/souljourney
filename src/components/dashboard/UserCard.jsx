f783a8e63f68d6997a0646653236d18f2772900e
ucangun <149247682+ucangun@users.noreply.github.com>
2025-03-19 15:38:28 +0100

    refactor: Update user data access in Notes, PatientList, and UserCard components for improved safety and readability

--- src/components/dashboard/UserCard.jsx
+++ src/components/dashboard/UserCard.jsx
@@ -1,32 +1,51 @@
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { format } from "date-fns";
+import useUserCall from "../../hooks/useUserCall";
+import useTherapistCall from "../../hooks/useTherapistCall";
+import { useEffect } from "react";
 
 export default function UserCard() {
+  const { getSingleUser } = useUserCall();
+  const { getSingleTherapist } = useTherapistCall();
   const { currentUser } = useSelector((state) => state.auth);
+  const { singleTherapist } = useSelector((state) => state.therapists);
+  const { singleUser } = useSelector((state) => state.users);
   const { t } = useTranslation();
+
+  useEffect(() => {
+    if (currentUser?._id) {
+      if (currentUser.isTherapist) {
+        getSingleTherapist(currentUser._id);
+      } else {
+        getSingleUser(currentUser._id);
+      }
+    }
+  }, [currentUser._id]);
+
+  const userData = currentUser.isTherapist ? singleTherapist : singleUser;
 
   return (
     <>
       <div className="w-full min-h-[200px] bg-gradient-to-bl from-navy-dark to-seaGreen-dark p-6 rounded-xl m-10 flex justify-between items-center">
         <div>
           <h1 className="text-3xl sm:text-5xl text-offWhite">
-            {currentUser.firstName.toUpperCase()}{" "}
-            {currentUser.lastName.toUpperCase()}
+            {userData?.firstName?.toUpperCase()}{" "}
+            {userData?.lastName?.toUpperCase()}
           </h1>
           <p className="mt-6 text-sm text-offWhite-dark w-52 md:w-full">
-            {currentUser.description}
+            {userData?.description}
           </p>
         </div>
         <div className="flex">
           <div className="shrink-1">
             <img
               src={
-                currentUser.image ||
+                userData?.image ||
                 "https://cdn-icons-png.flaticon.com/512/1053/1053244.png"
               }
               alt="Profile"
-              className="sm:w-[150px] sm:h-[150px] min-w-[120px]  rounded-full object-cover"
+              className="sm:w-[150px] sm:h-[150px] min-w-[120px] rounded-full object-cover"
             />
           </div>
         </div>
@@ -39,11 +58,10 @@ export default function UserCard() {
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
             <div>
               <p className="mb-1 font-semibold text-navy dark:text-offWhite">
-                {/* Email */}
                 {t("email")} :
               </p>
               <p className="font-medium text-navy dark:text-offWhite/60">
-                {currentUser?.email}
+                {userData?.email}
               </p>
             </div>
             <div>
@@ -51,7 +69,9 @@ export default function UserCard() {
                 {t("dateOfMembership")} :
               </p>
               <p className="font-medium text-navy dark:text-offWhite/60">
-                {format(new Date(currentUser?.createdAt), "dd.MM.yyyy")}
+                {userData?.createdAt
+                  ? format(new Date(userData?.createdAt), "dd.MM.yyyy")
+                  : "-"}
               </p>
             </div>
             <div>
@@ -59,7 +79,7 @@ export default function UserCard() {
                 {t("address")} :
               </p>
               <p className="font-medium text-navy dark:text-offWhite/60">
-                {currentUser?.address}
+                {userData?.address || "-"}
               </p>
             </div>
             <div>
@@ -67,7 +87,7 @@ export default function UserCard() {
                 {t("memberId")} :
               </p>
               <p className="font-medium text-navy dark:text-offWhite/60">
-                {currentUser?._id}
+                {userData?._id}
               </p>
             </div>
           </div>
