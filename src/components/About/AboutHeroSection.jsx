4f5dc84d9009307fcbd5633c98ea189100e86bd4
ucangun <149247682+ucangun@users.noreply.github.com>
2025-03-19 14:36:26 +0100

    refactor: Simplify structure and styling of AboutHeroSection for improved readability and responsiveness

--- src/components/About/AboutHeroSection.jsx
+++ src/components/About/AboutHeroSection.jsx
@@ -1,36 +1,24 @@
-import AboutHeroSectionBackground from "../../assets/AboutImages/Image1.png"
-import { useTranslation } from 'react-i18next';
+import AboutHeroSectionBackground from "../../assets/AboutImages/Image1.png";
+import { useTranslation } from "react-i18next";
 
 const AboutHeroSection = () => {
   const { t } = useTranslation();
   return (
     <div
-      className="relative w-full min-h-[60vh] bg-cover bg-center pt-24 pb-12"
+      className="relative w-full min-h-[50vh] bg-cover bg-center py-24 "
       style={{ backgroundImage: `url(${AboutHeroSectionBackground})` }}
     >
-      <div className="relative z-10 text-white text-center p-4 md:p-8 flex flex-col justify-center items-center h-full">
-        <h1
-          className="text-4xl sm:text-5xl md:text-6xl lg:text-[80px] font-bold w-full sm:w-[90%] md:w-[80%] mx-auto mb-4 md:mb-6 mt-14"
-          style={{
-            fontFamily: "'Source Serif Pro', serif",
-          }}
-        >
-          {t('AboutHeroSection.abt_us')}
+      <div className="relative z-10 flex flex-col items-center justify-center h-full p-4 text-center text-white md:p-8">
+        <h1 className="text-3xl font-bold md:text-4xl lg:text-6xl">
+          {t("AboutHeroSection.abt_us")}
         </h1>
 
-        <p
-          className="text-sm sm:text-base md:text-lg lg:text-xl mb-4 px-2 sm:px-4 md:px-12 w-full max-w-4xl mx-auto mt-8"
-          style={{
-            fontFamily: "'Urbanist', sans-serif",
-            fontSize: "clamp(0.875rem, 0.8rem + 0.5vw, 1.25rem)",
-          }}
-        >
-          {t('AboutHeroSection.abt_welcome')}
+        <p className="max-w-3xl px-4 mx-auto mt-4 text-lg lg:text-xl">
+          {t("AboutHeroSection.abt_welcome")}
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutHeroSection
-
+export default AboutHeroSection;
