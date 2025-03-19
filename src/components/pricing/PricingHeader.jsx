2b0e313dd24d9f12ae85545c1bb915c9fb27580c
ucangun <149247682+ucangun@users.noreply.github.com>
2025-03-19 14:26:39 +0100

    refactor: Improve formatting and structure of PricingHeader component for better readability

--- src/components/pricing/PricingHeader.jsx
+++ src/components/pricing/PricingHeader.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
 import { useTranslation } from "react-i18next";
-
 
 const PricingHeader = () => {
   const { t } = useTranslation();
   return (
     <div className="py-24 text-center bg-offWhite-dark dark:bg-background-darker ">
-        <h1  className="text-3xl md:text-4xl lg:text-5xl font-bold">{ t("paymentDetails")}</h1>
-        <p className = "max-w-3xl px-4 mx-auto mt-4 text-lg lg:text-xl">{ t("paymentDetailsText")}</p>
+      <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl">
+        {t("paymentDetails")}
+      </h1>
+      <p className="max-w-3xl px-4 mx-auto mt-4 text-lg lg:text-xl">
+        {t("paymentDetailsText")}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default PricingHeader
+export default PricingHeader;
