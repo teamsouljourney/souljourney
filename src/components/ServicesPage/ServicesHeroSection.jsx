d2d07b70220cdbeb4f5afd29a32c67f9c816b660
ucangun <149247682+ucangun@users.noreply.github.com>
2025-03-19 14:44:32 +0100

    refactor: Update styling of ServicesHeroSection for improved layout and text visibility

--- src/components/ServicesPage/ServicesHeroSection.jsx
+++ src/components/ServicesPage/ServicesHeroSection.jsx
@@ -1,38 +1,25 @@
-import Image1 from "../../assets/ServicesPageImages/Image1.png"
-import { useTranslation } from 'react-i18next';
-
+import Image1 from "../../assets/ServicesPageImages/Image1.png";
+import { useTranslation } from "react-i18next";
 
 const ServicesHeroSection = () => {
   const { t } = useTranslation();
 
   return (
     <div
-      className="relative w-full min-h-[60vh] bg-cover bg-center pt-24 pb-12"
+      className="w-full min-h-[55vh] bg-cover bg-center pt-36"
       style={{ backgroundImage: `url(${Image1})` }}
     >
-      <div className="relative z-10 text-white text-center p-4 md:p-8 flex flex-col justify-center items-center h-full">
-        <h1
-          className="text-4xl sm:text-5xl md:text-6xl lg:text-[90px] font-bold w-full sm:w-[90%] md:w-[80%] mx-auto mb-4 md:mb-6 mt-14"
-          style={{
-            fontFamily: "'Source Serif Pro', serif",
-          }}
-        >
+      <div className="text-center text-offWhite">
+        <h1 className="text-3xl font-bold md:text-4xl lg:text-6xl">
           {t("srvServicesHeroTitle")}
         </h1>
 
-        <p
-          className="text-sm sm:text-base md:text-lg lg:text-xl mb-4 px-2 sm:px-4 md:px-12 w-full max-w-4xl mx-auto mt-6"
-          style={{
-            fontFamily: "'Urbanist', sans-serif",
-            fontSize: "clamp(0.875rem, 0.8rem + 0.5vw, 1.25rem)",
-          }}
-        >
+        <p className="max-w-3xl px-4 mx-auto mt-4 text-lg lg:text-xl">
           {t("srvServicesHeroDescription")}
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ServicesHeroSection
-
+export default ServicesHeroSection;
